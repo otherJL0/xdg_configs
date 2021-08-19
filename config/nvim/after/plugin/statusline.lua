54f26d48ad3d535379bdd4d7d50863e6d2ca4c96
jon.lopez <jonathan.lopez@gridunity.com>
2021-08-19 14:57:41 -0400

    Adding basic statusline functionality

---
+++ config/nvim/after/plugin/statusline.lua
@@ -0,0 +1,59 @@
+local builtin = require('el.builtin')
+local extensions = require('el.extensions')
+local sections = require('el.sections')
+local subscribe = require('el.subscribe')
+local lsp_statusline = require('el.plugins.lsp_status')
+local helper = require('el.helper')
+
+local git_icon = subscribe.buf_autocmd("el_file_icon", "BufRead", function(_, bufnr)
+  local icon = extensions.file_icon(_, bufnr)
+  if icon then
+    return icon .. " "
+  end
+
+  return ""
+end)
+
+local git_branch = subscribe.buf_autocmd("el_git_branch", "BufEnter", function(window, buffer)
+  local branch = extensions.git_branch(window, buffer)
+  if branch then
+    return " " .. extensions.git_icon() .. " " .. branch
+  end
+end)
+
+local git_changes = subscribe.buf_autocmd("el_git_changes", "BufWritePost", function(window, buffer)
+  return extensions.git_changes(window, buffer)
+end)
+
+local buffer_percentage = function()
+    local fraction = math.floor(
+      100 * vim.api.nvim_win_get_cursor(0)[1] / vim.api.nvim_buf_line_count(0)
+    )
+    return string.format('%d%%%%', fraction)
+end
+
+require('el').setup({
+  generator = function(window, buffer)
+    local left = {
+      extensions.gen_mode({format_string =(' %s ') }),
+      git_icon,
+      git_branch,
+      git_changes
+    }
+
+    local middle = {
+    }
+
+    local right = {
+      '[',
+      builtin.line_with_width(3),
+      ':',
+      builtin.column_with_width(2),
+      ':',
+      buffer_percentage,
+      ']'
+    }
+
+    return vim.tbl_flatten({left, {sections.split}, middle, {sections.split}, right})
+  end
+})
