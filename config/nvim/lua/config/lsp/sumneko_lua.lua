7312ac984e19d0d9f7df2bf5b21503fbe7f9a3af
otherJL0 <jonathanglopez@gmail.com>
2021-08-20 22:19:41 -0400

    Fixing sumneko for lua

--- config/nvim/lua/config/lsp/sumneko_lua.lua
+++ config/nvim/lua/config/lsp/sumneko_lua.lua
@@ -2,11 +2,11 @@ local xdg_data_dir = vim.env.XDG_DATA_HOME or vim.env.HOME .. '/.local/share'
 local lua_language_server_dir = xdg_data_dir .. '/lua-language-server'
 
 local lua_language_server_cmd = {}
-for _, os in ipairs({'macOS', 'Linux', 'Windows'}) do
+for _, os in ipairs({'Linux', 'Windows'}) do
   local cmd_candidate = string.format('%s/bin/%s', lua_language_server_dir, os)
   if vim.fn.isdirectory(cmd_candidate) then
-    lua_language_server_cmd = { 
-      cmd_candidate .. '/lua-language-server', 
+    lua_language_server_cmd = {
+      cmd_candidate .. '/lua-language-server',
       lua_language_server_dir .. '/main.lua'
     }
     break
