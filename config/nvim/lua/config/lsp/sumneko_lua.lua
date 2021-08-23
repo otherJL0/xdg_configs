68f86a4a43c0b5ad91223c0dd4850f5d6fbedabf
otherJL0 <jonathanglopez@gmail.com>
2021-08-23 14:40:27 -0400

    Fixed issue with swithcig operating system

--- config/nvim/lua/config/lsp/sumneko_lua.lua
+++ config/nvim/lua/config/lsp/sumneko_lua.lua
@@ -2,16 +2,17 @@ local xdg_data_dir = vim.env.XDG_DATA_HOME or vim.env.HOME .. '/.local/share'
 local lua_language_server_dir = xdg_data_dir .. '/lua-language-server'
 local sumneko_cmd = lua_language_server_dir .. '/bin/'
 
-if vim.fn.has('mac') then
-  sumneko_cmd = sumneko_cmd .. 'macOS'
-elseif vim.fn.has('unix') then
-  sumneko_cmd = sumneko_cmd .. 'Linux'
+local operating_system
+if vim.fn.has('mac') == 1 then
+  operating_system = 'macOS'
+elseif vim.fn.has('unix') == 1 then
+  operating_system = 'Linux'
 end
 
 return require("lua-dev").setup({
   lspconfig = {
     cmd = {
-      sumneko_cmd .. '/lua-language-server',
+      sumneko_cmd .. operating_system .. '/lua-language-server',
       lua_language_server_dir .. '/main.lua',
       }
   },
