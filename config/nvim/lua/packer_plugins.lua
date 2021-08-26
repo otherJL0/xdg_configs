77df3cbd3b0ac83da8934fca6df31526c16891e9
otherJL0 <jonathanglopez@gmail.com>
2021-08-26 13:06:16 -0400

    Packer plugins added

--- config/nvim/lua/packer_plugins.lua
+++ config/nvim/lua/packer_plugins.lua
@@ -1,7 +1,15 @@
+local function my_plugins()
+  use({ "wbthomason/packer.nvim" })
+  use({ "neovim/nvim-lspconfig" })
+  use({ "nvim-telescope/telescope.nvim" })
+  use({ "nvim-lua/plenary.nvim" })
+  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+  use({ "tjdevries/astronauta.nvim" })
+  use({ "nvim-lua/lsp-status.nvim" })
+end
+
 return require("packer").startup({
-  function()
-    use({ "wbthomason/packer.nvim" })
-  end,
+  my_plugins,
   config = {
     display = {
       open_fn = function()
