b65eaa5bf831da94156008f4eaac66437343f925
otherJL0 <jonathanglopez@gmail.com>
2021-08-26 12:44:56 -0400

    Fixed packer install function

--- config/nvim/lua/config/setup/init.lua
+++ config/nvim/lua/config/setup/init.lua
@@ -1,11 +1,17 @@
 local M = {}
 
 function M.install_packer()
-  local prompt = vim.fn.input("No packer installation detected. Install packer now? (y/N)")
+  local prompt = vim.fn.input("No packer installation detected. Install packer now? (y/N) ")
+  vim.notify(".") -- clears prompt screen
   if prompt ~= "y" then
     vim.cmd("quitall!")
   end
-  vim.notify("installing packer...")
+  vim.notify("Installing packer...")
+  local packer_dir = vim.fn.stdpath("data") .. "/site/pack/packer/start/"
+  vim.fn.mkdir(packer_dir, "p")
+  local out = vim.fn.system(
+    string.format("git clone %s %s", "https://github.com/wbthomason/packer.nvim", packer_dir .. "packer.nvim")
+  )
 end
 
 return M
