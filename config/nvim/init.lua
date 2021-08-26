ce4d42bc48ef81215f0a3c39a312a6793ac72418
otherJL0 <jonathanglopez@gmail.com>
2021-08-26 12:41:08 -0400

    Adding initial packer install prompt

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,4 +2,5 @@ if pcall(require, "packer") == 1 then
   require("plugins")
 else
   require("config.setup").install_packer()
+  vim.notify("Packer installed")
 end
--- config/nvim/lua/config/setup/init.lua
+++ config/nvim/lua/config/setup/init.lua
@@ -1,7 +1,11 @@
 local M = {}
 
 function M.install_packer()
-  vim.notify("Attempt to install packer")
+  local prompt = vim.fn.input("No packer installation detected. Install packer now? (y/N)")
+  if prompt ~= "y" then
+    vim.cmd("quitall!")
+  end
+  vim.notify("installing packer...")
 end
 
 return M
