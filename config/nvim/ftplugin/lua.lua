66eee4bc47f03df45357af65434d31f7fab23a06
otherJL0 <jonathanglopez@gmail.com>
2021-08-31 14:44:49 -0400

    Clean up lua paths

--- config/nvim/ftplugin/lua.lua
+++ config/nvim/ftplugin/lua.lua
@@ -5,11 +5,10 @@ vim.opt.softtabstop = 2
 -- Add nvim/lua files to path
 vim.opt.path:append(vim.fn.stdpath("config") .. "/lua")
 -- Add packer fles to path
-vim.opt.path:append(vim.fn.stdpath("data") .. "/site/pack/packer/**/**/lua")
+vim.opt.path:append(vim.fn.stdpath("data") .. "/site/pack/packer/*/*/lua")
+vim.opt.suffixesadd:append("/init.lua")
 
 vim.opt.include = [[\v<((do|load)file|require)[^''"]*[''"]\zs[^''"]+]]
---vim.opt.includeexpr = string.gsub(vim.v.fname, "%.", "/")
--- vim.opt.includeexpr = vim.fn.substitute(vim.v.fname, "\\.", "/", "g")
 
 vim.cmd([[set includeexpr=substitute(v:fname,'\\.','/','g')]])
 vim.cmd([[
