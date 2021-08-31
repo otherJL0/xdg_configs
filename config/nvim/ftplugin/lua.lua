0f1356a5570be6ebacc9f8bde4f0e0bc2dc8921a
otherJL0 <jonathanglopez@gmail.com>
2021-08-31 14:29:07 -0400

    Properly set up includeexpr for lua

--- config/nvim/ftplugin/lua.lua
+++ config/nvim/ftplugin/lua.lua
@@ -5,8 +5,10 @@ vim.opt.softtabstop = 2
 -- Add nvim/lua files to path
 vim.opt.path:append(vim.fn.stdpath("config") .. "/lua")
 vim.opt.include = [[\v<((do|load)file|require)[^''"]*[''"]\zs[^''"]+]]
-vim.opt.includeexpr = vim.fn.substitute(vim.v.fname, "\\.", "/", "g")
+--vim.opt.includeexpr = string.gsub(vim.v.fname, "%.", "/")
+-- vim.opt.includeexpr = vim.fn.substitute(vim.v.fname, "\\.", "/", "g")
 
+vim.cmd([[set includeexpr=substitute(v:fname,'\\.','/','g')]])
 vim.cmd([[
   augroup StyluaAuto
     autocmd BufWritePre *.lua :lua require("config.stylua").format()
