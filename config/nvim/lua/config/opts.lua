09c0a9b9742576ae21b9c7c3ad7a84b7b46b799e
otherJL0 <jonathanglopez@gmail.com>
2021-08-31 13:27:45 -0400

    Adding more options

--- config/nvim/lua/config/opts.lua
+++ config/nvim/lua/config/opts.lua
@@ -6,10 +6,26 @@ opt.relativenumber = true
 opt.cursorlineopt = "both"
 opt.cursorline = true
 opt.expandtab = true
-opt.swapfile = false
+opt.swapfile = false -- No thank you
 opt.splitbelow = true
 opt.splitright = true
-opt.ignorecase = true
-opt.smartcase = true
+opt.ignorecase = true -- Ignore case when searching
+opt.smartcase = true -- Except when explicitly adding uppercase
 opt.signcolumn = "yes"
-opt.showtabline = 2
+opt.showtabline = 2 -- Always show tabline
+opt.clipboard = "unamedplus"
+opt.inccommand = "nosplit"
+opt.mouse = "n" -- Only allow mouse in normal mode
+opt.cmdheight = 1
+opt.scrolloff = 10
+
+-- Highlight on yank
+vim.api.nvim_exec(
+  [[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+  augroup end
+]],
+  false
+)
