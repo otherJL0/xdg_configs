053f8041c9b81fa48d4c6205dfa8510510b7e8f4
otherJL0 <jonathanglopez@gmail.com>
2021-09-01 08:42:27 -0400

    Add pcall to init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,9 +2,13 @@ if not pcall(require, "packer") then
   require("config.setup").install_packer()
 end
 
-require("packer_plugins")
+for _, plugin in pairs({ "impatient", "packer_plugins" }) do
+  if pcall(require, plugin) then
+    require(plugin)
+  end
+end
+
 vim.cmd("runtime plugin/astronauta.vim")
-require("impatient")
 require("config.opts")
 require("themes.onedark")
 
