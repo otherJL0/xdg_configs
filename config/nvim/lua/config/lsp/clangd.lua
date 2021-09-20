2580050091e0e208939c7556457499bef1c010cc
otherJL0 <jonathanglopez@gmail.com>
2021-09-20 10:33:23 -0400

    Added function to determine correct clangd command

--- config/nvim/lua/config/lsp/clangd.lua
+++ config/nvim/lua/config/lsp/clangd.lua
@@ -1,9 +1,25 @@
+local function get_clangd_cmd()
+  if vim.fn.executable("clangd") == 1 then
+    return "clangd"
+  end
+
+  -- Iterate backwards over latest clangd versions
+  for version = 15, 1, -1 do
+    local clangd_cmd = string.format("clangd-%d", version)
+    if vim.fn.executable(clangd_cmd) == 1 then
+      return clangd_cmd
+    end
+  end
+
+  return ""
+end
+
 return {
   init_options = {
     clangdFileStatus = true,
   },
   handlers = require("lsp-status").extensions.clangd.setup(),
-  cmd = { "clangd", "--background-index" },
+  cmd = { get_clangd_cmd(), "--background-index" },
   filetypes = {
     "c",
     "cpp",
