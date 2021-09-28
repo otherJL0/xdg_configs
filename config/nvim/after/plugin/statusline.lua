378429691302c96e911c5d6b309aa744ed225c20
jon.lopez <jonathan.lopez@gridunity.com>
2021-09-28 15:25:40 -0400

    Updating nvim options and cleaning up statusline

--- config/nvim/after/plugin/statusline.lua
+++ config/nvim/after/plugin/statusline.lua
@@ -1,1 +1,47 @@
-require("feline").setup()
+local vi_mode_utils = require("feline.providers.vi_mode")
+
+local active = {
+  left = {},
+  middle = {},
+  right = {},
+}
+
+active.left = {
+  {
+    provider = "vi_mode",
+    icon = "",
+    hl = function()
+      return {
+        name = vi_mode_utils.get_mode_highlight_name(),
+        bg = vi_mode_utils.get_mode_color(),
+        style = "none",
+      }
+    end,
+    right_sep = " ",
+  },
+
+  { provider = "git_branch" },
+  { provider = "git_diff_added" },
+  { provider = "git_diff_removed" },
+  { provider = "git_diff_changed" },
+}
+
+active.right = {
+  { provider = "position" },
+  { provider = "line_percentage" },
+}
+
+require("feline").setup({
+  components = {
+    active = {
+      active.left,
+      active.middle,
+      active.right,
+    },
+    inactive = {
+      {},
+      {},
+      {},
+    },
+  },
+})
--- config/nvim/lua/config/opts.lua
+++ config/nvim/lua/config/opts.lua
@@ -1,24 +1,47 @@
 local opt = vim.opt
 
+-- Wild menu
+opt.wildignore = opt.wildignore + { "__pycache__", "*.o", "*~", "*.pyc", "*pycache*" }
+opt.pumblend = 17
+opt.wildmode = opt.wildmode + { "longest", "full" }
+opt.wildoptions = "pum"
+
+-- Colorful
 opt.termguicolors = true
+
+-- Line numbers
 opt.number = true
 opt.relativenumber = true
+
+-- Tabs
+opt.autoindent = true
+opt.cindent = true
+opt.indentexpr = ""
+opt.wrap = true
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.expandtab = true
+
+-- Highlight current line and line number
 opt.cursorlineopt = "both"
 opt.cursorline = true
-opt.expandtab = true
-opt.swapfile = false -- No thank you
-opt.splitbelow = true
-opt.splitright = true
+
+opt.swapfile = false -- No swap files
+opt.splitbelow = true -- New splits open below
+opt.splitright = true --New vsplits open right
 opt.ignorecase = true -- Ignore case when searching
 opt.smartcase = true -- Except when explicitly adding uppercase
-opt.signcolumn = "yes"
+opt.signcolumn = "yes" -- Always show a sign column
 opt.showtabline = 2 -- Always show tabline
-opt.clipboard = "unnamedplus"
-opt.inccommand = "nosplit"
 opt.mouse = "n" -- Only allow mouse in normal mode
-opt.cmdheight = 1
-opt.scrolloff = 10
-opt.indentexpr = ""
+opt.scrolloff = 10 -- At least 10 lines should show past current line
+opt.showmode = false -- Do not show mode in cmd line
+opt.inccommand = "nosplit" -- Highlight search/replace queries
+opt.cmdheight = 1 -- Cmd height
+opt.showmatch = true -- Show matching bracket character when hovering
+opt.hidden = true -- Keep buffers around
 
 -- Highlight on yank
 vim.api.nvim_exec(
@@ -30,3 +53,22 @@ vim.api.nvim_exec(
 ]],
   false
 )
+
+opt.clipboard = "unnamedplus"
+
+opt.formatoptions = opt.formatoptions
+  - "a" -- Auto formatting is BAD.
+  - "t" -- Don't auto format my code. I got linters for that.
+  + "c" -- In general, I like it when comments respect textwidth
+  + "q" -- Allow formatting comments w/ gq
+  - "o" -- O and o, don't continue comments
+  + "r" -- But do continue when pressing enter.
+  + "n" -- Indent past the formatlistpat, not underneath it.
+  + "j" -- Auto-remove comments if possible.
+  - "2" -- I'm not in gradeschool anymore
+
+-- set joinspaces
+opt.joinspaces = false -- Two spaces and grade school, we're done
+
+-- set fillchars=eob:~
+opt.fillchars = { eob = "~" }
