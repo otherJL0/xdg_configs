3ed907683b5f7eb10b36a86085a34218cf659c52
otherJL0 <54177704+otherJL0@users.noreply.github.com>
2021-09-29 16:17:59 -0400

    Wrap all installs in a single script

--- scripts/os/zypper_install.sh
+++ scripts/os/zypper_install.sh
@@ -73,6 +73,50 @@ sudo zypper install apache-arrow-devel{,-static} \
   zlib-devel \
   zsh
 
+
+# Npm and yarn
+pkgs=(
+pyright
+@angular/language-server
+vscode-langservers-extracted
+dockerfile-language-server-nodejs
+elm
+elm-test
+elm-format
+@elm-tooling/elm-language-server
+typescript
+typescript-language-server
+vim-language-server
+vls
+)
+for pkg in ${pkgs[@]}; do
+    sudo npm install -g ${pkg}
+done
+
+yarn global add yaml-language-server
+yarn global add ansible-language-server
+
+# Pipx installs
+pipx_modules=(
+black
+flake8
+isort
+mypy
+mycli
+httpie
+cmake-language-server
+interrogate
+ipython
+ptpython
+tox
+pre-commit
+meson
+litecli
+)
+for module in ${pipx_modules[@]}; do 
+  pipx install ${module}
+done
+
 # Ad-hoc installs next
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 wget -O - https://github.com/digitalocean/doctl/releases/download/v1.64.0/doctl-1.64.0-linux-amd64.tar.gz | tar xzf -&& mv doctl ~/.local/bin/
@@ -80,3 +124,20 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 curl -LO https://get.golang.org/$(uname)/go_installer && chmod +x go_installer && ./go_installer && rm go_installer
 curl -fLo cs https://git.io/coursier-cli-"$(uname | tr LD ld)" && chmod +x cs && ./cs install cs && rm cs
 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+
+# Coursier installs
+~/.local/share/coursier/bin/cs java --jvm openjdk:1.11
+~/.local/share/coursier/bin/cs install scala metals bloop scala{fix,fmt,3,doc}
+
+# Go installs
+~/.go/bin/go install golang.org/x/tools/gopls@latest
+~/.go/bin/go install github.com/go-delve/delve/cmd/dlv@latest
+~/.go/bin/go install github.com/vultr/vultr-cli@latest
+~/.go/bin/go install github.com/mattn/efm-langserver@latest
+
+# Cargo installs
+~/.cargo/bin/cargo install stylua
+~/.cargo/bin/cargo install zoxide
+~/.cargo/bin/cargo install mcfly
+~/.cargo/bin/cargo install starship
+~/.cargo/bin/cargo install deno --locked
