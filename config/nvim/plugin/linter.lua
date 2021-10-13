a88f5872e26e5ce5e3fd6c5f4446d17a847275d5
otherJL0 <jonathanglopez@gmail.com>
2021-10-13 05:24:47 -0400

    Add shellcheck to lint table

--- config/nvim/plugin/linter.lua
+++ config/nvim/plugin/linter.lua
@@ -2,6 +2,7 @@ require("lint").linters_by_ft = {
   markdown = { "vale" },
   c = { "clangtidy", "clazy" },
   python = { "flake8", "mypy" },
+  sh = { "shellcheck" },
 }
 
 vim.cmd([[au BufWritePost <buffer> lua require('lint').try_lint()]])
