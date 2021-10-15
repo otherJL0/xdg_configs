e21414c95b7ef78fd36f4d020701d3ee97334784
otherJL0 <jonathanglopez@gmail.com>
2021-10-14 22:53:42 -0400

    Cleaned up statusline

--- config/nvim/after/plugin/statusline.lua
+++ config/nvim/after/plugin/statusline.lua
@@ -1,56 +1,108 @@
 local vi_mode_utils = require("feline.providers.vi_mode")
 
 local active = {
-  left = {},
-  middle = {},
-  right = {},
-}
-
-active.left = {
+  ----------
+  -- LEFT --
+  ----------
   {
-    provider = "vi_mode",
-    icon = "",
-    hl = function()
-      return {
-        name = vi_mode_utils.get_mode_highlight_name(),
-        bg = vi_mode_utils.get_mode_color(),
-        style = "none",
-      }
-    end,
-    right_sep = " ",
+    {
+      provider = "vi_mode",
+      icon = "",
+      hl = function()
+        return {
+          name = vi_mode_utils.get_mode_highlight_name(),
+          fg = vi_mode_utils.get_mode_color(),
+          style = "none",
+        }
+      end,
+      right_sep = " ",
+    },
+    { provider = "git_branch" },
+    { provider = "git_diff_added" },
+    { provider = "git_diff_removed" },
+    { provider = "git_diff_changed" },
   },
-  { provider = "file_type", right_sep = " " },
-  { provider = "git_branch" },
-  { provider = "git_diff_added" },
-  { provider = "git_diff_removed" },
-  { provider = "git_diff_changed" },
-}
 
-active.middle = {
+  ------------
+  -- MIDDLE --
+  ------------
   {
-    provider = function()
-      return require("lsp-status").status()
-    end,
+    {
+      provider = {
+        name = "file_info",
+        opts = {
+          type = "relative",
+        },
+      },
+    },
+  },
+
+  -----------
+  -- RIGHT --
+  -----------
+  {
+    {
+      provider = function()
+        return require("lsp-status").status()
+      end,
+    },
+    { provider = "lsp_client_names", right_sep = " " },
+    { provider = "position", right_sep = ":" },
+    { provider = "line_percentage" },
   },
 }
 
-active.right = {
-  { provider = "lsp_client_names", right_sep = " " },
-  { provider = "position", right_sep = ":" },
-  { provider = "line_percentage" },
+local inactive = {
+  ----------
+  -- LEFT --
+  ----------
+  {
+    {
+      provider = function()
+        return "-- INACTIVE --"
+      end,
+      hl = {
+        fg = "black",
+        bg = "white",
+      },
+    },
+  },
+  ------------
+  -- MIDDLE --
+  ------------
+  {
+    {
+      provider = {
+        name = "file_info",
+        opts = {
+          type = "relative",
+        },
+      },
+      hl = {
+        fg = "black",
+        bg = "white",
+      },
+    },
+  },
+  -----------
+  -- RIGHT --
+  -----------
+  {
+    {
+      provider = function()
+        return "-- INACTIVE --"
+      end,
+      hl = {
+        fg = "black",
+        bg = "white",
+      },
+    },
+  },
 }
 
 require("feline").setup({
   components = {
-    active = {
-      active.left,
-      active.middle,
-      active.right,
-    },
-    inactive = {
-      {},
-      {},
-      {},
-    },
+    active = active,
+    inactive = inactive,
   },
 })
