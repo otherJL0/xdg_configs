ea756c6d17c2750442dd1a3ae3d5da3230ac6107
otherJL0 <jonathanglopez@gmail.com>
2021-10-19 18:57:35 -0400

    Add On/Off options as toggleable

--- config/nvim/plugin/dial.lua
+++ config/nvim/plugin/dial.lua
@@ -15,9 +15,27 @@ dial.augends["custom#BOOL"] = dial.common.enum_cyclic({
   strlist = { "TRUE", "FALSE" },
 })
 
+dial.augends["custom#onoff"] = dial.common.enum_cyclic({
+  name = "boolean",
+  strlist = { "on", "off" },
+})
+
+dial.augends["custom#OnOff"] = dial.common.enum_cyclic({
+  name = "boolean",
+  strlist = { "On", "Off" },
+})
+
+dial.augends["custom#ONOFF"] = dial.common.enum_cyclic({
+  name = "boolean",
+  strlist = { "ON", "OFF" },
+})
+
 table.insert(dial.config.searchlist.normal, "custom#bool")
 table.insert(dial.config.searchlist.normal, "custom#Bool")
 table.insert(dial.config.searchlist.normal, "custom#BOOL")
+table.insert(dial.config.searchlist.normal, "custom#onoff")
+table.insert(dial.config.searchlist.normal, "custom#OnOff")
+table.insert(dial.config.searchlist.normal, "custom#ONOFF")
 
 vim.cmd([[nmap <C-a> <Plug>(dial-increment)]])
 vim.cmd([[nmap <C-x> <Plug>(dial-decrement)]])
