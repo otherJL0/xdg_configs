aa42ae6b8697597fd1ff08dfa9daba528fd50628
otherJL0 <jonathanglopez@gmail.com>
2021-10-28 11:46:42 -0400

    Remove unecessary clangd path finding function

--- config/nvim/lua/config/lsp/clangd.lua
+++ config/nvim/lua/config/lsp/clangd.lua
@@ -1,25 +1,9 @@
-local function get_clangd_cmd()
-  if vim.fn.executable("clangd") == 1 then
-    return "clangd"
-  end
-
-  -- Iterate backwards over latest clangd versions
-  for version = 15, 1, -1 do
-    local clangd_cmd = string.format("clangd-%d", version)
-    if vim.fn.executable(clangd_cmd) == 1 then
-      return clangd_cmd
-    end
-  end
-
-  return ""
-end
-
 return {
   init_options = {
     clangdFileStatus = true,
   },
   handlers = require("lsp-status").extensions.clangd.setup(),
-  cmd = { get_clangd_cmd(), "--background-index" },
+  cmd = { "clangd", "--background-index" },
   filetypes = {
     "c",
     "cpp",
