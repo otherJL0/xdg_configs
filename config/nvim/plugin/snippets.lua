8a92f7259ff3dce6cdbc012176211d7e1d65d58d
otherJL0 <jonathanglopez@gmail.com>
2021-10-29 13:27:19 -0400

    Adding snippets keybindings

--- config/nvim/plugin/snippets.lua
+++ config/nvim/plugin/snippets.lua
@@ -1,13 +1,14 @@
-local ls = require("luasnip")
-local snippets = ls.snippets
-local snippet = ls.snippet
-local sn = ls.snippet_node
-local isn = ls.indent_snippet_node
-local t = ls.text_node
-local i = ls.insert_node
-local f = ls.function_node
-local c = ls.choice_node
-local d = ls.dynamic_node
+local inoremap = vim.keymap.inoremap
+local luasnip = require("luasnip")
+local snippets = luasnip.snippets
+local snippet = luasnip.snippet
+local sn = luasnip.snippet_node
+local isn = luasnip.indent_snippet_node
+local t = luasnip.text_node
+local i = luasnip.insert_node
+local f = luasnip.function_node
+local c = luasnip.choice_node
+local d = luasnip.dynamic_node
 local events = require("luasnip.util.events")
 
 snippets.lua = {
@@ -32,5 +33,15 @@ snippets.python = {
       [[if __name__ == "__main__":]],
       "    main()",
     }),
+  }),
+  snippet({ trig = "sqltable", descr = "class Table(SQLModel, table=True)" }, {
+    t({ "class " }),
+    i(1),
+    t({
+      "(SQLModel, table=True):",
+      "    id: Optional[int] = Field(default=None, primary_key=True)",
+      "    ",
+    }),
+    i(0),
   }),
 }
