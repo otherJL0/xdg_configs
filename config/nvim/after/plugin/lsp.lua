e0525c755bc024ce77ccd0f19ddebff8df5d2f72
otherJL0 <jonathanglopez@gmail.com>
2021-11-04 16:39:38 -0400

    Adding prettierd to null-ls

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -81,13 +81,14 @@ local null_ls = require("null-ls")
 -- register any number of sources simultaneously
 local sources = {
   null_ls.builtins.code_actions.gitsigns,
-  null_ls.builtins.formatting.black,
-  null_ls.builtins.formatting.isort,
-  null_ls.builtins.formatting.shfmt,
-  null_ls.builtins.formatting.shellharden,
-  null_ls.builtins.formatting.stylua,
   null_ls.builtins.diagnostics.flake8,
   null_ls.builtins.diagnostics.hadolint,
+  null_ls.builtins.formatting.black,
+  null_ls.builtins.formatting.isort,
+  null_ls.builtins.formatting.prettierd,
+  null_ls.builtins.formatting.shellharden,
+  null_ls.builtins.formatting.shfmt,
+  null_ls.builtins.formatting.stylua,
 }
 
 null_ls.config({ sources = sources })
