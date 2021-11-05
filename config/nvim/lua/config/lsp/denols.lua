bec4a67777d54dd4213b77875ade3b5d9c6b41de
otherJL0 <jonathanglopez@gmail.com>
2021-11-05 16:25:59 -0400

    Updating deno language server configs

--- config/nvim/lua/config/lsp/denols.lua
+++ config/nvim/lua/config/lsp/denols.lua
@@ -16,12 +16,18 @@ return {
     codeLens = {
       implementations = true,
       references = true,
+      referencesAllFunctions = true,
+      test = true,
     },
     suggest = {
       autoImports = true,
       completeFunctionCalls = true,
       names = true,
       paths = true,
+      imports = {
+        autoDiscover = true,
+        hosts = true,
+      },
     },
   },
   root_dir = require("lspconfig.util").root_pattern("denops/", "package.json", "tsconfig.json", "mod.ts", "app.tsx"),
