1f9851fdabc2a23d0cd53d437dc0800ed105fbcb
otherJL0 <jonathanglopez@gmail.com>
2021-11-15 23:31:19 -0500

    Use tmux environment variable for notification

--- config/nvim/plugin/tmux.lua
+++ config/nvim/plugin/tmux.lua
@@ -1,7 +1,8 @@
 local uv = vim.loop
 local pipe = uv.new_pipe(true)
 
-pipe:connect("/private/tmp/tmux-501/default")
+local tmux_socket = vim.split(vim.env.TMUX, ",")[1]
+pipe:connect(tmux_socket)
 vim.notify(pipe:getpeername())
 pipe:listen(128, function()
   local client = uv.new_pipe(false)
