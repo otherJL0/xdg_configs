699fe25315c95fb4f199eeb0349035f779a3c304
otherJL0 <jonathanglopez@gmail.com>
2021-11-17 20:46:41 +0000

    Only show tmux socket if tmux env variable is set

--- config/nvim/plugin/tmux.lua
+++ config/nvim/plugin/tmux.lua
@@ -1,12 +1,14 @@
 local uv = vim.loop
 local pipe = uv.new_pipe(true)
 
-local tmux_socket = vim.split(vim.env.TMUX, ",")[1]
-pipe:connect(tmux_socket)
-vim.notify(pipe:getpeername())
-pipe:listen(128, function()
-  local client = uv.new_pipe(false)
-  pipe:accept(client)
-  client:write("hello!\n")
-  client:close()
-end)
+if vim.env.TMUX then
+  local tmux_socket = vim.split(vim.env.TMUX, ",")[1]
+  pipe:connect(tmux_socket)
+  vim.notify(pipe:getpeername())
+  pipe:listen(128, function()
+    local client = uv.new_pipe(false)
+    pipe:accept(client)
+    client:write("hello!\n")
+    client:close()
+  end)
+end
