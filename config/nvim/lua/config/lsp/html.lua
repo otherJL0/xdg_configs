b09c16164d2d05de3b20ccc5772c4aa5e589cdab
otherJL0 <jonathanglopez@gmail.com>
2021-11-22 15:39:28 -0500

    Adding css and html language server configs

---
+++ config/nvim/lua/config/lsp/html.lua
@@ -0,0 +1,16 @@
+--Enable (broadcasting) snippet capability for completion
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+return {
+  cmd = { "vscode-html-language-server", "--stdio" },
+  filetypes = { "html" },
+  init_options = {
+    configurationSection = { "html", "css", "javascript" },
+    embeddedLanguages = {
+      css = true,
+      javascript = true,
+    },
+  },
+  single_file_support = true,
+}
