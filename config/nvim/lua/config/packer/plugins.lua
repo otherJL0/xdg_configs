b053b64c4a9031a92e9d4b16fdd89dd172439a56
otherJL0 <jonathanglopez@gmail.com>
2021-12-14 14:43:16 -0500

    Upgrading cmdline

--- config/nvim/lua/config/packer/plugins.lua
+++ config/nvim/lua/config/packer/plugins.lua
@@ -29,6 +29,49 @@ local function my_plugins()
     "ggandor/lightspeed.nvim",
   })
 
+  use({ "tpope/vim-rsi" })
+  use({
+    "vonheikemen/fine-cmdline.nvim",
+    config = function()
+      require("fine-cmdline").setup({
+        cmdline = {
+          enable_keymaps = true,
+          smart_history = false,
+        },
+        popup = {
+          position = {
+            row = "10%",
+            col = "50%",
+          },
+          size = {
+            width = "60%",
+            height = 1,
+          },
+          border = {
+            style = "rounded",
+            highlight = "FloatBorder",
+          },
+          win_options = {
+            winhighlight = "Normal:Normal",
+          },
+        },
+        hooks = {
+          before_mount = function(input)
+            -- code
+          end,
+          after_mount = function(input)
+            -- code
+          end,
+          set_keymaps = function(imap, feedkeys)
+            -- code
+          end,
+        },
+      })
+      vim.api.nvim_set_keymap("n", "<CR>", "<cmd>FineCmdline<CR>", { noremap = true })
+      vim.api.nvim_set_keymap("n", ":", "<cmd>FineCmdline<CR>", { noremap = true })
+    end,
+  })
+
   -- LSP Extras
   use({
     "nvim-lua/lsp-status.nvim",
