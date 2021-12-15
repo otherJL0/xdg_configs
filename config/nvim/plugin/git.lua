4d759df56ca355bc09891ae18e96d512149b5497
otherJL0 <jonathanglopez@gmail.com>
2021-12-15 08:54:25 -0500

    Update git plugin configs

--- config/nvim/plugin/git.lua
+++ config/nvim/plugin/git.lua
@@ -5,12 +5,44 @@ local neogit = require("neogit")
 
 diffview.setup({
   diff_binaries = false, -- Show diffs for binaries
+  enhanced_diff_hl = true, -- See ':h diffview-config-enhanced_diff_hl'
   use_icons = true, -- Requires nvim-web-devicons
+  icons = { -- Only applies when use_icons is true.
+    folder_closed = "",
+    folder_open = "",
+  },
+  signs = {
+    fold_closed = "",
+    fold_open = "",
+  },
   file_panel = {
     position = "left", -- One of 'left', 'right', 'top', 'bottom'
     width = 35, -- Only applies when position is 'left' or 'right'
     height = 10, -- Only applies when position is 'top' or 'bottom'
+    listing_style = "tree", -- One of 'list' or 'tree'
+    tree_options = { -- Only applies when listing_style is 'tree'
+      flatten_dirs = true, -- Flatten dirs that only contain one single dir
+      folder_statuses = "only_folded", -- One of 'never', 'only_folded' or 'always'.
+    },
   },
+  file_history_panel = {
+    position = "bottom",
+    width = 35,
+    height = 16,
+    log_options = {
+      max_count = 256, -- Limit the number of commits
+      follow = false, -- Follow renames (only for single file)
+      all = false, -- Include all refs under 'refs/' including HEAD
+      merges = false, -- List only merge commits
+      no_merges = false, -- List no merge commits
+      reverse = false, -- List commits in reverse order
+    },
+  },
+  default_args = { -- Default args prepended to the arg-list for the listed commands
+    DiffviewOpen = {},
+    DiffviewFileHistory = {},
+  },
+  hooks = {},
   key_bindings = {
     disable_defaults = false, -- Disable the default key bindings
     -- The `view` bindings are active in the diff buffers, only when the current
@@ -18,6 +50,9 @@ diffview.setup({
     view = {
       ["<tab>"] = cb("select_next_entry"), -- Open the diff for the next file
       ["<s-tab>"] = cb("select_prev_entry"), -- Open the diff for the previous file
+      ["gf"] = cb("goto_file"), -- Open the file in a new split in previous tabpage
+      ["<C-w><C-f>"] = cb("goto_file_split"), -- Open the file in a new split
+      ["<C-w>gf"] = cb("goto_file_tab"), -- Open the file in a new tabpage
       ["<leader>e"] = cb("focus_files"), -- Bring focus to the files panel
       ["<leader>b"] = cb("toggle_files"), -- Toggle the files panel.
     },
@@ -36,17 +71,53 @@ diffview.setup({
       ["R"] = cb("refresh_files"), -- Update stats and entries in the file list.
       ["<tab>"] = cb("select_next_entry"),
       ["<s-tab>"] = cb("select_prev_entry"),
+      ["gf"] = cb("goto_file"),
+      ["<C-w><C-f>"] = cb("goto_file_split"),
+      ["<C-w>gf"] = cb("goto_file_tab"),
+      ["i"] = cb("listing_style"), -- Toggle between 'list' and 'tree' views
+      ["f"] = cb("toggle_flatten_dirs"), -- Flatten empty subdirectories in tree listing style.
       ["<leader>e"] = cb("focus_files"),
       ["<leader>b"] = cb("toggle_files"),
+    },
+    file_history_panel = {
+      ["g!"] = cb("options"), -- Open the option panel
+      ["<C-A-d>"] = cb("open_in_diffview"), -- Open the entry under the cursor in a diffview
+      ["y"] = cb("copy_hash"), -- Copy the commit hash of the entry under the cursor
+      ["zR"] = cb("open_all_folds"),
+      ["zM"] = cb("close_all_folds"),
+      ["j"] = cb("next_entry"),
+      ["<down>"] = cb("next_entry"),
+      ["k"] = cb("prev_entry"),
+      ["<up>"] = cb("prev_entry"),
+      ["<cr>"] = cb("select_entry"),
+      ["o"] = cb("select_entry"),
+      ["<2-LeftMouse>"] = cb("select_entry"),
+      ["<tab>"] = cb("select_next_entry"),
+      ["<s-tab>"] = cb("select_prev_entry"),
+      ["gf"] = cb("goto_file"),
+      ["<C-w><C-f>"] = cb("goto_file_split"),
+      ["<C-w>gf"] = cb("goto_file_tab"),
+      ["<leader>e"] = cb("focus_files"),
+      ["<leader>b"] = cb("toggle_files"),
+    },
+    option_panel = {
+      ["<tab>"] = cb("select"),
+      ["q"] = cb("close"),
     },
   },
 })
 
 neogit.setup({
   disable_signs = false,
+  disable_hint = false,
   disable_context_highlighting = false,
   disable_commit_confirmation = false,
-  -- customize displayed signs
+  autorefresh = true,
+  disable_builtin_notifications = false,
+  commit_popup = {
+    kind = "split",
+  },
+  kind = "split",
   signs = {
     -- { CLOSED, OPENED }
     section = { ">", "v" },
@@ -55,6 +126,29 @@ neogit.setup({
   },
   integrations = {
     diffview = true,
+  },
+  sections = {
+    untracked = {
+      folded = false,
+    },
+    unstaged = {
+      folded = false,
+    },
+    staged = {
+      folded = false,
+    },
+    stashes = {
+      folded = true,
+    },
+    unpulled = {
+      folded = true,
+    },
+    unmerged = {
+      folded = false,
+    },
+    recent = {
+      folded = true,
+    },
   },
   -- override/add mappings
   mappings = {
@@ -70,16 +164,16 @@ neogit.setup({
 
 gitsigns.setup({
   signs = {
-    add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    add = { hl = "GitSignsAdd", text = "+", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+    change = { hl = "GitSignsChange", text = "Δ", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    delete = { hl = "GitSignsDelete", text = "-", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
     topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
     changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
   },
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-  numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+  numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
   linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
-  word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+  word_diff = true, -- Toggle with `:Gitsigns toggle_word_diff`
   keymaps = {
     -- Default keymap options
     noremap = true,
@@ -110,7 +204,7 @@ gitsigns.setup({
   current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
   current_line_blame_opts = {
     virt_text = true,
-    virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+    virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
     delay = 1000,
   },
   current_line_blame_formatter_opts = {
