04ad6b49a0ca55e5b05bb215977c001b41e3bd11
otherJL0 <jonathanglopez@gmail.com>
2021-12-16 15:30:25 -0500

    Adding initial wren queries

---
+++ config/nvim/queries/wren/highlights.scm
@@ -0,0 +1,31 @@
+(num) @number
+(string) @string
+(bool) @boolean
+
+(comment) @comment
+
+
+[
+ "as"
+ "break"
+ "class"
+ "construct"
+ "continue"
+ "else"
+ "false"
+ "for"
+ "foreign"
+ "if"
+ "import"
+ "in"
+ "is"
+ "null"
+ "return"
+ "static"
+ "super"
+ "this"
+ "true"
+ "var"
+ "while"
+ ] @keyword
+
