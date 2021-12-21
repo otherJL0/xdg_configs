120e3c8bd89dcf23293c32ccf753008bc0d8723a
otherJL0 <jonathanglopez@gmail.com>
2021-12-21 07:43:58 -0500

    Allow black to format python structural matches

--- config/nvim/lua/config/packer/plugins.lua
+++ config/nvim/lua/config/packer/plugins.lua
@@ -50,9 +50,11 @@ local function my_plugins()
       require("null-ls").setup({
         sources = {
           require("null-ls").builtins.formatting.stylua,
-          require("null-ls").builtins.formatting.black,
           require("null-ls").builtins.formatting.isort,
           require("null-ls").builtins.formatting.shfmt,
+          require("null-ls").builtins.formatting.black.with({
+            extra_args = { "----target-version", "py310" },
+          }),
           require("null-ls").builtins.diagnostics.mypy,
           require("null-ls").builtins.diagnostics.hadolint,
           require("null-ls").builtins.diagnostics.flake8,
