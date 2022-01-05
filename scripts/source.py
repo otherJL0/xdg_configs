d759bae9334e2204a20c2b562e0356a6d4f0eeda
otherJL0 <jonathanglopez@gmail.com>
2022-01-05 08:51:23 -0500

    Allow overwriting exsiting files in xdg directory

--- scripts/source.py
+++ scripts/source.py
@@ -1,14 +1,55 @@
+import shutil
+import sys
 from pathlib import Path
+
+
+def get_path_type(path: Path) -> str:
+    if path.is_symlink():
+        return "symlink"
+    if path.is_dir():
+        return "dir"
+    if path.is_file():
+        return "file"
+    print(f"Unknown file type for {path}")
+    sys.exit(1)
+
+
+def prompt_replacement(target: Path) -> bool:
+    type = get_path_type(target)
+    remove_existing = ""
+    while remove_existing not in ["y", "n", "Y", "N"]:
+        remove_existing = input(f"Remove existing {type} {target}? (y/N) ")
+    return remove_existing in ["Y", "y"]
+
+
+def remove_existing_path(target: Path) -> None:
+    match get_path_type(target):
+        case "symlink" | "file":
+            target.unlink()
+            print(f"{target} unlinked")
+        case "dir":
+            shutil.rmtree(target)
+            print(f"Dir {target} removed")
+        case _:
+            print("Unknown file type")
+            sys.exit(1)
 
 
 def main() -> None:
     configs = Path.cwd() / "config"
     xdg_config_dir = Path.home() / ".config"
     for dir in configs.iterdir():
+        target = xdg_config_dir / dir.name
         try:
-            dir.symlink_to(xdg_config_dir)
+            target.symlink_to(dir)
+            print(f"Symlink created for {dir}")
         except FileExistsError:
-            print(f"Link exists for {dir.name}")
+            if target.is_symlink() and target.readlink() == dir:
+                print(f"\tLink exists for {target}")
+            else:
+                if prompt_replacement(target):
+                    remove_existing_path(target)
+                    dir.symlink_to(xdg_config_dir)
 
 
 if __name__ == "__main__":
