44838adeeddd911d29aac064db3808c9efe5aaca
otherJL0 <jonathanglopez@gmail.com>
2022-01-05 10:09:07 -0500

    Clean up init file

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,20 +1,22 @@
 if not pcall(require, "packer") then
   require("config.setup").install_packer()
 else
-  require("fun")()
   vim.g.mapleader = " "
+  vim.g.did_load_filetypes = 0
+  vim.g.do_filetype_lua = 1
+  vim.g.minor_mode = ""
+
+  require("fun")()
+
   require("impatient").enable_profile()
   vim.notify = require("notify")
   require("config.packer")
-
   require("config.globals")
 
   vim.cmd("runtime plugin/astronauta.vim")
 
   require("themes.catppuccin")
   require("config.minor_modes")
-
-  vim.g.minor_mode = ""
 
   function P(element)
     print(vim.inspect(element))
