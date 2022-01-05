3673ad8a64b90541d9209a094ecf03a6146f6c35
otherJL0 <jonathanglopez@gmail.com>
2022-01-05 13:36:03 -0500

    Change fish path order

--- config/fish/conf.d/path.fish
+++ config/fish/conf.d/path.fish
@@ -4,20 +4,10 @@ set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx XDG_CACHE_HOME $HOME/.cache
 
-# user path
-fish_add_path -pP $HOME/.local/bin
-fish_add_path -pP $HOME/.cargo/bin
-fish_add_path -pP $HOME/.jbang/bin
-fish_add_path -pP $HOME/.jbang/currentjdk/bin
-fish_add_path -pP $HOME/.ghcup/bin
-fish_add_path -pP $HOME/.yarn/bin
-fish_add_path -pP $HOME/.deno/bin
-fish_add_path -pP $HOME/bin
-fish_add_path -pP $HOME/.luarocks/bin
-fish_add_path -pP ./node_modules/.bin
-fish_add_path -pP ./.venv/bin
-fish_add_path -pP $HOME/.cache/coursier/jvm/openjdk@1.11.0-2/bin
-fish_add_path -pP $HOME/.cache/nvim/packer_hererocks/2.1.0-beta3/bin
+# Rust
+set -gx RUSTUP_HOME $HOME/.rustup
+set -gx RUSTUP_TOOLCHAIN nightly-x86_64-unknown-linux-gnu
+set -ga fish_user_paths $RUSTUP_HOME/toolchains/$RUSTUP_TOOLCHAIN/bin
 
 # Neovim paths
 set -gx NVIM_DATA $XDG_DATA_HOME/nvim
@@ -51,7 +41,17 @@ set -gx GOMODCACHE $XDG_CACHE_HOME/gomod
 set -gx fish_user_paths $GOBIN
 
 
-# Rust
-set -gx RUSTUP_HOME $HOME/.rustup
-set -gx RUSTUP_TOOLCHAIN nightly-x86_64-unknown-linux-gnu
-set -ga fish_user_paths $RUSTUP_HOME/toolchains/$RUSTUP_TOOLCHAIN/bin
+# user path
+fish_add_path -pP $HOME/.local/bin
+fish_add_path -pP $HOME/.cargo/bin
+fish_add_path -pP $HOME/.jbang/bin
+fish_add_path -pP $HOME/.jbang/currentjdk/bin
+fish_add_path -pP $HOME/.ghcup/bin
+fish_add_path -pP $HOME/.yarn/bin
+fish_add_path -pP $HOME/.deno/bin
+fish_add_path -pP $HOME/bin
+fish_add_path -pP $HOME/.luarocks/bin
+fish_add_path -pP ./node_modules/.bin
+fish_add_path -pP ./.venv/bin
+fish_add_path -pP $HOME/.cache/coursier/jvm/openjdk@1.11.0-2/bin
+fish_add_path -pP $HOME/.cache/nvim/packer_hererocks/2.1.0-beta3/bin
