c38e83f175b95375535cd4708d377d33a618848e
otherJL0 <jonathanglopez@gmail.com>
2022-01-08 21:29:23 -0500

    Add rustup toolchain to path

--- config/fish/conf.d/path.fish
+++ config/fish/conf.d/path.fish
@@ -4,11 +4,6 @@ set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx XDG_CACHE_HOME $HOME/.cache
 
-# Rust
-# set -gx RUSTUP_HOME $HOME/.rustup
-# set -gx RUSTUP_TOOLCHAIN nightly-x86_64-unknown-linux-gnu
-# set -ga fish_user_paths $RUSTUP_HOME/toolchains/$RUSTUP_TOOLCHAIN/bin
-
 # Neovim paths
 set -gx NVIM_DATA $XDG_DATA_HOME/nvim
 set -gx NVIM_CACHE $XDG_CACHE_HOME/nvim
@@ -55,3 +50,8 @@ fish_add_path -pP ./node_modules/.bin
 fish_add_path -pP ./.venv/bin
 fish_add_path -pP $HOME/.cache/coursier/jvm/openjdk@1.11.0-2/bin
 fish_add_path -pP $HOME/.cache/nvim/packer_hererocks/2.1.0-beta3/bin
+
+# Rust
+set -gx RUSTUP_HOME $HOME/.rustup
+set -gx RUSTUP_TOOLCHAIN nightly-x86_64-unknown-linux-gnu
+set -ga fish_user_paths $RUSTUP_HOME/toolchains/$RUSTUP_TOOLCHAIN/bin
