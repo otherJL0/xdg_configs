7324138bb93da185f412eec8fb690fe425729a6e
otherJL0 <jonathanglopez@gmail.com>
2022-01-10 08:37:30 -0500

    Cleaned up fish paths

--- config/fish/conf.d/path.fish
+++ config/fish/conf.d/path.fish
@@ -17,7 +17,8 @@ set -gx JVM_VERSION 1.11.0-2
 set -gx COURSIER_CACHE $XDG_CACHE_HOME/coursier
 set -gx COURSIER_DATA $XDG_DATA_HOME/coursier
 set -gx JAVA_HOME $COURSIER_CACHE/jvm/$JVM_VENDOR@$JVM_VERSION
-set -ga fish_user_paths $COURSIER_DATA/bin
+fish_add_path -P $COURSIER_DATA/bin
+# set -ga fish_user_paths $COURSIER_DATA/bin
 
 # Clang
 set -gx CC clang
@@ -33,25 +34,27 @@ set -gx CMAKE_DISABLE32BIT ON
 set -gx GOBIN $HOME/.go/bin
 set -gx GOCACHE $XDG_CACHE_HOME/go
 set -gx GOMODCACHE $XDG_CACHE_HOME/gomod
-set -gx fish_user_paths $GOBIN
+fish_add_path --prepend --path --global $GOBIN
+# set -gx fish_user_paths $GOBIN
 
 
 # user path
-fish_add_path -pP $HOME/.local/bin
-fish_add_path -pP $HOME/.cargo/bin
-fish_add_path -pP $HOME/.jbang/bin
-fish_add_path -pP $HOME/.jbang/currentjdk/bin
-fish_add_path -pP $HOME/.ghcup/bin
-fish_add_path -pP $HOME/.yarn/bin
-fish_add_path -pP $HOME/.deno/bin
-fish_add_path -pP $HOME/bin
-fish_add_path -pP $HOME/.luarocks/bin
-fish_add_path -pP ./node_modules/.bin
-fish_add_path -pP ./.venv/bin
-fish_add_path -pP $HOME/.cache/coursier/jvm/openjdk@1.11.0-2/bin
-fish_add_path -pP $HOME/.cache/nvim/packer_hererocks/2.1.0-beta3/bin
+fish_add_path -P $HOME/.local/bin
+fish_add_path -P $HOME/.cargo/bin
+fish_add_path -P $HOME/.jbang/bin
+fish_add_path -P $HOME/.jbang/currentjdk/bin
+fish_add_path -P $HOME/.ghcup/bin
+fish_add_path -P $HOME/.yarn/bin
+fish_add_path -P $HOME/.deno/bin
+fish_add_path -P $HOME/bin
+fish_add_path -P $HOME/.luarocks/bin
+fish_add_path -P ./node_modules/.bin
+fish_add_path -P ./.venv/bin
+fish_add_path -P $HOME/.cache/coursier/jvm/openjdk@1.11.0-2/bin
+fish_add_path -P $HOME/.cache/nvim/packer_hererocks/2.1.0-beta3/bin
 
 # Rust
 set -gx RUSTUP_HOME $HOME/.rustup
 set -gx RUSTUP_TOOLCHAIN nightly-x86_64-unknown-linux-gnu
-set -ga fish_user_paths $RUSTUP_HOME/toolchains/$RUSTUP_TOOLCHAIN/bin
+fish_add_path --prepend --path --global $RUSTUP_HOME/toolchains/$RUSTUP_TOOLCHAIN/bin
+# set -ga fish_user_paths $RUSTUP_HOME/toolchains/$RUSTUP_TOOLCHAIN/bin
