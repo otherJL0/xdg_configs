d734d65a29164ff4c5aa084a80206d7729ad5c09
otherJL0 <jonathanglopez@gmail.com>
2022-01-10 20:52:08 -0500

    Adding aocc to fish path

---
+++ config/fish/conf.d/setenv_AOCC.fish
@@ -0,0 +1,7 @@
+fish_add_path --prepend --path --global /opt/AMD/aocc-compiler-3.2.0.sles15/bin
+set --prepend LIBRARY_PATH /opt/AMD/aocc-compiler-3.2.0.sles15/lib /opt/AMD/aocc-compiler-3.2.0.sles15/lib32 /usr/lib/x86_64-linux-gnu /usr/lib64 /usr/lib32 /usr/lib
+set --prepend LD_LIBRARY_PATH /opt/AMD/aocc-compiler-3.2.0.sles15/ompd /opt/AMD/aocc-compiler-3.2.0.sles15/lib /opt/AMD/aocc-compiler-3.2.0.sles15/lib32 /usr/lib/x86_64-linux-gnu /usr/lib64 /usr/lib32 /usr/lib
+set --prepend C_INCLUDE_PATH /opt/AMD/aocc-compiler-3.2.0.sles15/include
+set --prepend CPLUS_INCLUDE_PATH /opt/AMD/aocc-compiler-3.2.0.sles15/include
+set --prepend LIBRARY_PATH /usr/lib64 /usr/lib
+set --prepend LD_LIBRARY_PATH /usr/lib64 /usr/lib
