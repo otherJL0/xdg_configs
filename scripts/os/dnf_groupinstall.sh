ad51c2b2989dedb23186af48c6f14045bca53f10
otherJL0 <jonathanglopez@gmail.com>
2022-01-18 22:09:14 -0500

    Remove D tools

--- scripts/os/dnf_groupinstall.sh
+++ scripts/os/dnf_groupinstall.sh
@@ -1,6 +1,5 @@
 sudo dnf groupinstall -y --with-optional \
   "C Development Tools and Libraries" \
-  "D Development Tools and Libraries" \
   "Development Tools" \
   "Container Management" \
   "System Tools"
