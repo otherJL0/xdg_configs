fd95e22892f284327a9c4bf1f1f1b065dda69b30
otherJL0 <jonathanglopez@gmail.com>
2022-01-21 09:33:05 -0500

    Fish command correction attempt

---
+++ config/fish/conf.d/common_mispelling.fish
@@ -0,0 +1,13 @@
+abbr -a rmd mkdir
+abbr -a rmi mkdir
+abbr -a rmr mkdir
+abbr -a rmdi mkdir
+abbr -a rmdr mkdir
+abbr -a rmir mkdir
+
+abbr -a mkd mkdir
+abbr -a mki mkdir
+abbr -a mkr mkdir
+abbr -a mkdi mkdir
+abbr -a mkdr mkdir
+abbr -a mkir mkdir
