380118cb4275ff8049ea3cc8a4166d450fd0980e
otherJL0 <jonathanglopez@gmail.com>
2022-03-01 10:21:52 -0500

    Installing yarn and charmbracelet apps

--- scripts/npm.fish
+++ scripts/npm.fish
@@ -1,5 +1,8 @@
+npm install -g yarn
+yarn global add yaml-language-server
+
 # Npm 
-set pkgs @angular/language-server @elm-tooling/elm-language-server bash-language-server dockerfile-language-server-nodejs elm elm-format elm-test pyright typescript typescript-language-server vim-language-server vls vscode-langservers-extracted
+set pkgs @angular/language-server @elm-tooling/elm-language-server bash-language-server dockerfile-language-server-nodejs elm elm-format elm-test pyright typescript typescript-language-server vim-language-server vls vscode-langservers-extracted tree-sitter-cli
 
 for pkg in $pkgs
     npm install -g $pkg
--- scripts/tools/go_installs.sh
+++ scripts/tools/go_installs.sh
@@ -1,2 +1,7 @@
 go install golang.org/x/tools/gopls@latest
 go install github.com/go-delve/delve/cmd/dlv@latest
+
+# Charmbracelet Tools
+go install github.com/charmbracelet/skate@latest
+go install github.com/charmbracelet/soft-serve/cmd/soft@latest
+go install github.com/charmbracelet/glow@latest
