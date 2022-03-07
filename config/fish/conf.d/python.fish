57478fea687e9b76ac38ed50d40b76ef12f214b7
otherJL0 <jonathanglopez@gmail.com>
2022-03-07 10:56:15 -0500

    Fixed typo in poetry environment variables

--- config/fish/conf.d/python.fish
+++ config/fish/conf.d/python.fish
@@ -2,8 +2,8 @@ abbr -a venv py -m venv .venv
 abbr -a activate . ./.venv/bin/activate.fish
 
 # Poetry environment variables
-set -gx POETRY_EXPERIMENTAL_NEW-INSTALLER true
+set -gx POETRY_EXPERIMENTAL_NEW_INSTALLER true
 set -gx POETRY_INSTALLER_PARALLEL true
 set -gx POETRY_VIRTUALENVS_CREATE true
-set -gx POETRY_VIRTUALENVS_IN-PROJECT true
+set -gx POETRY_VIRTUALENVS_IN_PROJECT true
 set -gx POETRY_VIRTUALENVS_PATH .venv
