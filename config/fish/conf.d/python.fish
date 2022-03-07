8b955f237f89e9500e5d66ed4c3dd1f31edc99ef
otherJL0 <jonathanglopez@gmail.com>
2022-03-07 10:55:03 -0500

    Adding poetry environment variables

--- config/fish/conf.d/python.fish
+++ config/fish/conf.d/python.fish
@@ -1,2 +1,9 @@
 abbr -a venv py -m venv .venv
 abbr -a activate . ./.venv/bin/activate.fish
+
+# Poetry environment variables
+set -gx POETRY_EXPERIMENTAL_NEW-INSTALLER true
+set -gx POETRY_INSTALLER_PARALLEL true
+set -gx POETRY_VIRTUALENVS_CREATE true
+set -gx POETRY_VIRTUALENVS_IN-PROJECT true
+set -gx POETRY_VIRTUALENVS_PATH .venv
