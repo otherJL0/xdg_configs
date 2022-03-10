ac80529d90502b0a468e04e0b0b16cfe9af5a723
otherJL0 <jonathanglopez@gmail.com>
2022-03-10 11:08:31 -0500

    First pass at creating ls/exa fish aliases

---
+++ config/fish/conf.d/aliases.fish
@@ -0,0 +1,55 @@
+function ls --wraps ls --description 'alias ls'
+    argparse t/time r/reverse l/list a/all S/blocks -- $argv
+    or return # exit if argparse failed because it found an option it didn't recognize - it will print an error
+
+    set flags --icons
+
+    if set -q _flag_blocks
+        set flags $flags --blocks
+    end
+
+    if set -q _flag_list
+        set flags $flags -l
+    end
+
+    if set -q _flag_all
+        set flags $flags -a
+    end
+
+    if set -q _flag_reverse
+        set flags $flags --reverse
+    end
+
+    # if -t is given
+    if set -q _flag_time
+        set flags $flags --sort time
+    end
+
+    exa $flags $argv
+end
+
+function ll --wraps ls --description 'alias ll'
+    argparse t/time r/reverse a/all l/list S/blocks -- $argv
+    or return # exit if argparse failed because it found an option it didn't recognize - it will print an error
+
+    set flags --icons -l
+
+    if set -q _flag_blocks
+        set flags $flags --blocks
+    end
+
+    if set -q _flag_all
+        set flags $flags -a
+    end
+
+    if set -q _flag_reverse
+        set flags $flags --reverse
+    end
+
+    # if -t is given
+    if set -q _flag_time
+        set flags $flags --sort time
+    end
+
+    exa $flags $argv
+end
