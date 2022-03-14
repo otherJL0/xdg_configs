00af078e6fb939ef23dc6933c001d2de19aa3f78
otherJL0 <jonathanglopez@gmail.com>
2022-03-14 10:59:05 -0400

    Updating kitty and fish interop

--- config/fish/conf.d/gh.fish
+++ config/fish/conf.d/gh.fish
@@ -1,8 +1,19 @@
-function ghclone -d "Clone repo based on relative path"
-    set -l _current_path (string split / (pwd))
+# function ghclone -d "Clone repo based on relative path"
+#     set -l _current_path (string split / (pwd))
 
+#     if test $_current_path[-2] = gh
+#         set -l target_repo $_current_path[-1]/$argv
+#         gh repo clone $target_repo
+#     end
+# end
+function _gh_organization --on-event PWD
+    set -l _current_path ""
     if test $_current_path[-2] = gh
-        set -l target_repo $_current_path[-1]/$argv
-        gh repo clone $target_repo
+        set -gx GH_ORG $_current_path[-1]
+    else
+        set -gx GH_ORG ""
     end
+
 end
+
+abbr --add ghclone gh repo clone $GH_ORG
--- config/fish/conf.d/path.fish
+++ config/fish/conf.d/path.fish
@@ -6,6 +6,22 @@ set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx XDG_CACHE_HOME $HOME/.cache
 
+# user path
+fish_add_path -P $HOME/.local/bin
+fish_add_path -P $HOME/.cargo/bin
+fish_add_path -P $HOME/.jbang/bin
+fish_add_path -P $HOME/.jbang/currentjdk/bin
+fish_add_path -P $HOME/.ghcup/bin
+fish_add_path -P $HOME/.yarn/bin
+fish_add_path -P $HOME/.deno/bin
+fish_add_path -P $HOME/bin
+fish_add_path -P $HOME/.luarocks/bin
+fish_add_path -P ./node_modules/.bin
+fish_add_path -P ./.venv/bin
+fish_add_path -P $HOME/.cache/coursier/jvm/openjdk@1.11.0-2/bin
+fish_add_path -P $HOME/.cache/nvim/packer_hererocks/2.1.0-beta3/bin
+fish_add_path -P /usr/local/bin/
+
 # Neovim paths
 set -gx NVIM_DATA $XDG_DATA_HOME/nvim
 set -gx NVIM_CACHE $XDG_CACHE_HOME/nvim
@@ -54,22 +70,6 @@ set -gx GOCACHE $XDG_CACHE_HOME/go
 set -gx GOMODCACHE $XDG_CACHE_HOME/gomod
 fish_add_path --prepend --path --global $GOBIN
 # set -gx fish_user_paths $GOBIN
-
-
-# user path
-fish_add_path -P $HOME/.local/bin
-fish_add_path -P $HOME/.cargo/bin
-fish_add_path -P $HOME/.jbang/bin
-fish_add_path -P $HOME/.jbang/currentjdk/bin
-fish_add_path -P $HOME/.ghcup/bin
-fish_add_path -P $HOME/.yarn/bin
-fish_add_path -P $HOME/.deno/bin
-fish_add_path -P $HOME/bin
-fish_add_path -P $HOME/.luarocks/bin
-fish_add_path -P ./node_modules/.bin
-fish_add_path -P ./.venv/bin
-fish_add_path -P $HOME/.cache/coursier/jvm/openjdk@1.11.0-2/bin
-fish_add_path -P $HOME/.cache/nvim/packer_hererocks/2.1.0-beta3/bin
 
 # Rust
 set -gx RUSTUP_HOME $HOME/.rustup
