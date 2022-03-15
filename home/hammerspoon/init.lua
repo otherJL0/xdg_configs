cddb3d5594166592d0a47a67c0ebb6fdbbea5d1d
otherJL0 <jonathanglopez@gmail.com>
2022-03-15 10:14:59 -0400

    Refactored hammerspoon tiling script

--- home/hammerspoon/init.lua
+++ home/hammerspoon/init.lua
@@ -1,109 +1,110 @@
--- Table containing functions to resize current window
-local tile_window = {
-	-- Resize window to left 50% of screen
-	left = function()
+local function tile_window(direction)
+	return function()
 		local window = hs.window.focusedWindow()
+
+		-- Used for left/right tiling
 		local screen = window:screen()
 		local window_layout = window:frame():toUnitRect(screen:frame())
+
+		-- Used for top/bottom tiling
+		local frame = window:frame()
+		local max = screen:frame()
 
 		-- If the window is already tiled to the left 50% of the screen
 		-- and a monitor exists to the left of this widow,
 		-- move window to the right half of the left monitor
-		if window_layout:equals(hs.layout.left50) and screen:toWest() then
-			window:moveOneScreenWest(false, true)
-			window:move(hs.layout.right50)
-		else
-			window:move(hs.layout.left50)
+		if direction == "left" then
+			if window_layout:equals(hs.layout.left50) and screen:toWest() then
+				window:moveOneScreenWest(false, true)
+				window:move(hs.layout.right50)
+			else
+				window:move(hs.layout.left50)
+			end
+			return
 		end
-	end,
-
-	-- Resize window to right 50% of screen
-	right = function()
-		local window = hs.window.focusedWindow()
-		local screen = window:screen()
-		local window_layout = window:frame():toUnitRect(screen:frame())
 
 		-- If the window is already tiled to the right 50% of the screen
 		-- and a monitor exists to the right of this widow,
 		-- move window to the left half of the right monitor
-		if window_layout:equals(hs.layout.right50) and screen:toEast() then
-			window:moveOneScreenEast(false, true)
-			window:move(hs.layout.left50)
-		else
-			window:move(hs.layout.right50)
+		if direction == "right" then
+			if window_layout:equals(hs.layout.right50) and screen:toEast() then
+				window:moveOneScreenEast(false, true)
+				window:move(hs.layout.left50)
+			else
+				window:move(hs.layout.right50)
+			end
+			return
 		end
-	end,
 
-	-- Resize current window to half its height without changing its wiedth
-	-- if the current window is taking up the left 50% of the screen,
-	-- resizing with `top` changes current window to top left 25% of screen
-	-- if the current window takes up the whole screen,
-	-- resizing with `top` changes current window to top 50% of screen
-	top = function()
-		local window = hs.window.focusedWindow()
-		local frame = window:frame()
-		local screen = window:screen()
-		local max = screen:frame()
+		-- Resize current window to half its height without changing its width
+		-- if the current window is taking up the left 50% of the screen,
+		-- resizing with `top` changes current window to top left 25% of screen
+		-- if the current window takes up the whole screen,
+		-- resizing with `top` changes current window to top 50% of screen
+		if direction == "top" then
+			frame.y = max.y
+			frame.h = max.h / 2
+			window:setFrame(frame)
+			return
+		end
 
-		frame.y = max.y
-		frame.h = max.h / 2
-		window:setFrame(frame)
-	end,
+		-- Resize current window to half its height without changing its width
+		-- if the current window is taking up the left 50% of the screen,
+		-- resizing with `bottom` changes current window to bottom left 25% of screen
+		-- if the current window takes up the whole screen,
+		-- resizing with `bottom` changes current window to bottom 50% of screen
+		if direction == "bottom" then
+			frame.y = max.y + (max.h / 2)
+			frame.h = max.h / 2
+			window:setFrame(frame)
+			return
+		end
+	end
+end
 
-	-- Resize current window to half its height without changing its wiedth
-	-- if the current window is taking up the left 50% of the screen,
-	-- resizing with `bottom` changes current window to bottom left 25% of screen
-	-- if the current window takes up the whole screen,
-	-- resizing with `bottom` changes current window to bottom 50% of screen
-	bottom = function()
-		local window = hs.window.focusedWindow()
-		local frame = window:frame()
-		local screen = window:screen()
-		local max = screen:frame()
+local function maximize_window()
+	local window = hs.window.focusedWindow()
+	window:move(hs.layout.maximized)
+end
 
-		frame.y = max.y + (max.h / 2)
-		frame.h = max.h / 2
-		window:setFrame(frame)
-	end,
+local function change_focus(direction)
+	return function()
+		local window = hs.window.focusedWindow()
 
-	-- Maximize application without going full screen
-	maximize = function()
-		local window = hs.window.focusedWindow()
-		window:move(hs.layout.maximized)
-	end,
-}
+		if direction == "left" then
+			window:focusWindowWest(nil, false, false)
+			return
+		end
 
-local focus = {
-	left = function()
-		local window = hs.window.focusedWindow()
-		window:focusWindowWest(nil, false, false)
-	end,
-	right = function()
-		local window = hs.window.focusedWindow()
-		window:focusWindowEast(nil, false, false)
-	end,
-	top = function()
-		local window = hs.window.focusedWindow()
-		window:focusWindowNorth(nil, false, false)
-	end,
-	bottom = function()
-		local window = hs.window.focusedWindow()
-		window:focusWindowSouth(nil, false, false)
-	end,
-}
+		if direction == "right" then
+			window:focusWindowEast(nil, false, false)
+			return
+		end
+
+		if direction == "top" then
+			window:focusWindowNorth(nil, false, false)
+			return
+		end
+
+		if direction == "bottom" then
+			window:focusWindowSouth(nil, false, false)
+			return
+		end
+	end
+end
 
 -- Window Resizing
-hs.hotkey.bind({ "cmd", "shift" }, "H", tile_window.left)
-hs.hotkey.bind({ "cmd", "shift" }, "L", tile_window.right)
-hs.hotkey.bind({ "cmd", "shift" }, "K", tile_window.top)
-hs.hotkey.bind({ "cmd", "shift" }, "J", tile_window.bottom)
-hs.hotkey.bind({ "cmd", "shift" }, "O", tile_window.maximize)
+hs.hotkey.bind({ "cmd", "shift" }, "H", tile_window("left"))
+hs.hotkey.bind({ "cmd", "shift" }, "L", tile_window("right"))
+hs.hotkey.bind({ "cmd", "shift" }, "K", tile_window("top"))
+hs.hotkey.bind({ "cmd", "shift" }, "J", tile_window("bottom"))
+hs.hotkey.bind({ "cmd", "shift" }, "O", maximize_window)
 
 -- Window Focus
-hs.hotkey.bind({ "cmd" }, "H", focus.left)
-hs.hotkey.bind({ "cmd" }, "L", focus.right)
-hs.hotkey.bind({ "cmd" }, "K", focus.top)
-hs.hotkey.bind({ "cmd" }, "J", focus.bottom)
+hs.hotkey.bind({ "cmd" }, "H", change_focus("left"))
+hs.hotkey.bind({ "cmd" }, "L", change_focus("right"))
+hs.hotkey.bind({ "cmd" }, "K", change_focus("top"))
+hs.hotkey.bind({ "cmd" }, "J", change_focus("bottom"))
 
 hs.hotkey.bind({ "cmd", "shift" }, "R", hs.reload)
 
