1b342de038655d0089ac8e3a81ffeb38591c758f
otherJL0 <jonathanglopez@gmail.com>
2022-03-21 11:23:07 -0400

    Refactor function to add more description

--- home/hammerspoon/init.lua
+++ home/hammerspoon/init.lua
@@ -1,58 +1,58 @@
+-- Load EmmyLua annotations for language server auto-completion
 hs.loadSpoon("EmmyLua")
 
 local function tile_window(direction)
 	return function()
 		local window = hs.window.focusedWindow()
 
-		-- Used for left/right tiling
-		local screen = window:screen()
-		local window_layout = window:frame():toUnitRect(screen:frame())
+		-- Which screen/monitor the current window is on
+		local window_screen = window:screen()
 
-		-- Used for top/bottom tiling
-		local frame = window:frame()
-		local max = screen:frame()
+		-- The dimensions of the current screen/monitor
+		local screen_frame = window_screen:frame()
 
-		-- If the window is already tiled to the left 50% of the screen
-		-- and a monitor exists to the left of this widow,
-		-- move window to the right half of the left monitor
+		-- The dimensions of the current screen
+		local window_frame = window:frame()
+
+		-- The ratio of the current window frame in relation to its screen/monitor
+		local window_layout = window_frame:toUnitRect(screen_frame)
+
 		if direction == "left" then
-			if window_layout:equals(hs.layout.left50) and screen:toWest() then
-				window:moveOneScreenWest(false, true)
+			-- If the window is already tiled to the left 50% of the screen
+			-- and a monitor exists to the left of this widow,
+			-- move window to the right half of the left monitor
+			if window_layout:equals(hs.layout.left50) and window_screen:toWest() then
+				window:moveOneScreenWest({ noResize = false, ensureInScreenBounds = true })
 				window:move(hs.layout.right50)
 			else
 				window:move(hs.layout.left50)
 			end
-
+		elseif direction == "right" then
 			-- If the window is already tiled to the right 50% of the screen
 			-- and a monitor exists to the right of this widow,
 			-- move window to the left half of the right monitor
-		elseif direction == "right" then
-			if window_layout:equals(hs.layout.right50) and screen:toEast() then
-				window:moveOneScreenEast(false, true)
+			if window_layout:equals(hs.layout.right50) and window_screen:toEast() then
+				window:moveOneScreenEast({ noResize = false, ensureInScreenBounds = true })
 				window:move(hs.layout.left50)
 			else
 				window:move(hs.layout.right50)
 			end
-
+		elseif direction == "top" then
 			-- Resize current window to half its height without changing its width
 			-- if the current window is taking up the left 50% of the screen,
 			-- resizing with `top` changes current window to top left 25% of screen
 			-- if the current window takes up the whole screen,
 			-- resizing with `top` changes current window to top 50% of screen
-		elseif direction == "top" then
-			frame.y = max.y
-			frame.h = max.h / 2
-			window:setFrame(frame)
-
-			-- Resize current window to half its height without changing its width
-			-- if the current window is taking up the left 50% of the screen,
-			-- resizing with `bottom` changes current window to bottom left 25% of screen
-			-- if the current window takes up the whole screen,
-			-- resizing with `bottom` changes current window to bottom 50% of screen
+			window_frame.y = screen_frame.y
+			window_frame.h = screen_frame.h / 2
+			window:setFrame(window_frame)
 		elseif direction == "bottom" then
-			frame.y = max.y + (max.h / 2)
-			frame.h = max.h / 2
-			window:setFrame(frame)
+			-- If the current window's height is less than half the height of the screen and
+			-- Resize current window to half its height without changing its width
+			-- and move window to the bottom part of the screen
+			window_frame.y = screen_frame.y + (screen_frame.h / 2)
+			window_frame.h = screen_frame.h / 2
+			window:setFrame(window_frame)
 		end
 	end
 end
@@ -62,18 +62,18 @@ local function maximize_window()
 	window:move(hs.layout.maximized)
 end
 
-local function change_focus(direction)
+local function change_window_focus(direction)
 	return function()
 		local window = hs.window.focusedWindow()
 
 		if direction == "left" then
-			window:focusWindowWest(nil, false, false)
+			window:focusWindowWest({ candidateWindows = nil, frontmost = false, strict = false })
 		elseif direction == "right" then
-			window:focusWindowEast(nil, false, false)
+			window:focusWindowEast({ candidateWindows = nil, frontmost = false, strict = false })
 		elseif direction == "top" then
-			window:focusWindowNorth(nil, false, false)
+			window:focusWindowNorth({ candidateWindows = nil, frontmost = false, strict = false })
 		elseif direction == "bottom" then
-			window:focusWindowSouth(nil, false, false)
+			window:focusWindowSouth({ candidateWindows = nil, frontmost = false, strict = false })
 		end
 	end
 end
@@ -86,10 +86,10 @@ hs.hotkey.bind({ "cmd", "shift" }, "J", tile_window("bottom"))
 hs.hotkey.bind({ "cmd", "shift" }, "O", maximize_window)
 
 -- Window Focus
-hs.hotkey.bind({ "cmd" }, "H", change_focus("left"))
-hs.hotkey.bind({ "cmd" }, "L", change_focus("right"))
-hs.hotkey.bind({ "cmd" }, "K", change_focus("top"))
-hs.hotkey.bind({ "cmd" }, "J", change_focus("bottom"))
+hs.hotkey.bind({ "cmd" }, "H", change_window_focus("left"))
+hs.hotkey.bind({ "cmd" }, "L", change_window_focus("right"))
+hs.hotkey.bind({ "cmd" }, "K", change_window_focus("top"))
+hs.hotkey.bind({ "cmd" }, "J", change_window_focus("bottom"))
 
 hs.hotkey.bind({ "cmd", "shift" }, "R", hs.reload)
 
