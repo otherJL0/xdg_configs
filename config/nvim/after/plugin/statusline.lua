861860cc9a2e1709eedaac13caee897f79652dfe
otherJL0 <jonathanglopez@gmail.com>
2022-03-21 17:15:58 -0400

    Make window separators more obvious

--- config/nvim/after/plugin/statusline.lua
+++ config/nvim/after/plugin/statusline.lua
@@ -1,3 +1,19 @@
+vim.api.nvim_exec(
+  [[
+hi WinSeparator  guifg=#96CDFB
+]],
+  false
+)
+vim.opt.fillchars = {
+  horiz = "━",
+  horizup = "┻",
+  horizdown = "┳",
+  vert = "┃",
+  vertleft = "┫",
+  vertright = "┣",
+  verthoriz = "╋",
+}
+
 -- Set global status line
 vim.opt.laststatus = 3
 require("config.statusline.express_line")
