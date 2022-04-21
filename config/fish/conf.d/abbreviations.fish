7d1666098428655bcc4ea7c30f0f2cf750a568e4
Jonathan Lopez <jonathanglopez@gmail.com>
2022-04-21 08:19:53 -0400

    Cleaning up git abbreviations

--- config/fish/conf.d/abbreviations.fish
+++ config/fish/conf.d/abbreviations.fish
@@ -1,26 +1,29 @@
-abbr -a gA git add -A
+abbr -a gR git restore .
 abbr -a gA git add -A
 abbr -a ga git add
-abbr -a gaA git add
 abbr -a gb git branch
 abbr -a gbD git branch -D
-abbr -a gc git commit
-abbr -a gcam git commit -am
 abbr -a gch git checkout
+abbr -a gc git commit
 abbr -a gcm git commit -m
+abbr -a gcam git commit -am
 abbr -a gcma git commit -ma
 abbr -a gdiff git diff
-abbr -a gdifftool git difftool
 abbr -a gl git log
+abbr -a gmv git mv
+abbr -a patch git add --patch
 abbr -a gph git push
 abbr -a gpl git pull
+abbr -a grm git rm -r --cached
 abbr -a gs git status
 abbr -a gss git status -s
-abbr -a gmv git mv
-abbr -a grm git rm -r --cached
 abbr -a gsub git submodule
 abbr -a gsubup git submodule update
 abbr -a gsupir git submodule update --init --recursive
-abbr -a gR git restore .
+abbr -a gsw git switch
+
 abbr -a branch git symbolic-ref --short HEAD
+abbr -a difftool git difftool
+abbr -a mergetool git mergetool
+abbr -a unstage git reset HEAD --
 abbr -a upstream git push --set-upstream origin (git symbolic-ref --short HEAD)
