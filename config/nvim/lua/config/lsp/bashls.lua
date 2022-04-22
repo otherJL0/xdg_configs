dd0ffcb8736964daf1d637519d816647830b37c3
Jonathan Lopez <jonathanglopez@gmail.com>
2022-04-22 11:45:51 -0400

    Restore bash-language-server

--- config/nvim/lua/config/lsp/bashls.lua
+++ config/nvim/lua/config/lsp/bashls.lua
@@ -1,7 +1,7 @@
 return {
   cmd = { vim.fn.stdpath("cache") .. "/node_modules/.bin/" .. "bash-language-server", "start" },
-  cmd_env = {
-    GLOB_PATTERN = "*@(.sh|.inc|.bash|.command)",
-  },
+  -- cmd_env = {
+  --   GLOB_PATTERN = "*@(.sh|.inc|.bash|.command)",
+  -- },
   filetypes = { "sh", "bash" },
 }
