2e8118a15dd236e1aab85ca0d8ec3c2155a09e0d
Jonathan Lopez <jonathanglopez@gmail.com>
2022-05-03 14:34:39 -0400

    Fix requirements highlights

--- config/nvim/queries/requirements/highlights.scm
+++ config/nvim/queries/requirements/highlights.scm
@@ -1,12 +1,15 @@
 (name) @namespace
-(version_cmp) @bool
 (version) @float
 (extra) @literal
 (comment) @comment
 (url) @text.uri
 (local_path) @text.uri
+(version_cmp) @operator
+
+; (option) @string.special
+(option) @label
 
 (str) @string
 
 ["," ";"] @punctuation.delimiter
-["[" "]"]@punctuation.bracket
+["[" "]"] @punctuation.bracket
