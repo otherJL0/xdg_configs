d89211bd011de3f2daada7847ee41919b4db5b5d
Jonathan Lopez <jonathanglopez@gmail.com>
2022-05-04 09:21:48 -0400

    WIP gh user commands

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -100,3 +100,43 @@ vim.keymap.set("n", " fj", ivy.file_browser)
 vim.keymap.set("n", " fm", ivy.man_pages)
 vim.keymap.set("n", "gI", ivy.lsp_implementations)
 vim.keymap.set("n", " fp", ivy.project)
+
+vim.api.nvim_create_user_command("Gh", function(opts)
+  local fargs = opts.fargs
+  -- require("telescope.command").load_command(unpack(opts.fargs))
+end, {
+  nargs = "*",
+  complete = function(_, line)
+    local actions = {
+      ["pull_request"] = { "author", "assignee", "label", "search", "state", "base", "limit" },
+      ["issue"] = { "author", "assignee", "mention", "label", "milestone", "search", "state", "limit" },
+    }
+
+    local l = vim.split(line, "%s+")
+    local n = #l - 2
+
+    if n == 0 then
+      return vim.tbl_filter(function(val)
+        return vim.startswith(val, l[2])
+      end, vim.tbl_extend("force", actions, extensions_list))
+    end
+
+    if n == 1 then
+      local is_extension = vim.tbl_filter(function(val)
+        return val == l[2]
+      end, extensions_list)
+
+      if #is_extension > 0 then
+        local extensions_subcommand_dict = require("telescope.command").get_extensions_subcommand()
+        return vim.tbl_filter(function(val)
+          return vim.startswith(val, l[3])
+        end, extensions_subcommand_dict[l[2]])
+      end
+    end
+
+    local options_list = vim.tbl_keys(require("telescope.config").values)
+    return vim.tbl_filter(function(val)
+      return vim.startswith(val, l[#l])
+    end, options_list)
+  end,
+})
