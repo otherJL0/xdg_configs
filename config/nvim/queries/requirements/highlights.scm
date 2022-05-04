208446a4426c03e4dd88334f60d8d21720ad1ba4
Jonathan Lopez <jonathanglopez@gmail.com>
2022-05-03 21:49:04 -0400

    Updating highlight rules

--- config/nvim/queries/requirements/highlights.scm
+++ config/nvim/queries/requirements/highlights.scm
@@ -1,15 +1,14 @@
 (name) @namespace
 (version) @float
-(extra) @literal
+(extra) @include
 (comment) @comment
 (url) @text.uri
 (local_path) @text.uri
 (version_cmp) @operator
 
-; (option) @string.special
 (option) @label
 
 (str) @string
 
-["," ";"] @punctuation.delimiter
-["[" "]"] @punctuation.bracket
+[ "," ";" ] @punctuation.delimiter
+[ "[" "]" ] @punctuation.bracket
