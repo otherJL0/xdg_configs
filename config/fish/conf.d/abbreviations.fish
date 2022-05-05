4271b5c0408d6b41fb0bd61336e1c3d23f6c1513
Jonathan Lopez <jonathanglopez@gmail.com>
2022-05-05 13:52:10 -0400

    Adding rg and fd abbr

--- config/fish/conf.d/abbreviations.fish
+++ config/fish/conf.d/abbreviations.fish
@@ -36,3 +36,13 @@ abbr -a pym python -m
 abbr -a Type type -a
 abbr -a wcl wc -l
 abbr -a prs gh pr status
+
+abbr -a fdd fd --type d
+abbr -a fdf fd --type f
+abbr -a fdl fd --type l
+abbr -a fdex fd --extension
+abbr -a fdjson fd --extension json
+abbr -a fdpy fd --extension py
+
+
+abbr -a rgpy rg --type py
