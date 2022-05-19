687ce1dc5e568657ca3ef2fbeade35425738f3fb
Jonathan Lopez <jonathanglopez@gmail.com>
2022-05-19 13:08:48 -0400

    Updating neovide configs

--- config/nvim/plugin/neovide.lua
+++ config/nvim/plugin/neovide.lua
@@ -1,2 +1,7 @@
-vim.g.neovide_transparency = 0.8
+-- vim.g.neovide_transparency = 0.8
 vim.g.neovide_input_use_logo = false
+-- vim.opt.guifont = "JetBrains Mono Regular Nerd Font Complete Mono:h12"
+-- vim.opt.guifont = "JetBrains Mono Nerd Font"
+vim.opt.guifont = "JetBrainsMono Nerd Font Mono:h11"
+vim.g.guioptions = "aceimrlb"
+vim.g.neovide_cursor_vfx_mode = "sonicboom"
