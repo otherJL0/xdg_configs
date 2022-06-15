bfad7eb3f88f97628c5a6aeb5b2507e9d6511461
Jonathan Lopez <jonathanglopez@gmail.com>
2022-06-14 20:36:16 -0400

    Cleaning up lsp configs

--- config/nvim/after/ftplugin/python.lua
+++ config/nvim/after/ftplugin/python.lua
@@ -1,14 +1,14 @@
-local function determine_venv_path()
-  local cwd = vim.fn.getcwd()
-  local candidate_path
-  for _, venv in ipairs({ "venv", ".venv", "env" }) do
-    candidate_path = string.format("%s/%s/bin/python", cwd, venv)
-    if vim.fn.executable(candidate_path) == 1 then
-      return venv
-    end
-  end
-  return vim.env.HOME .. "/.asdf/shims/python3"
-end
+-- local function determine_venv_path()
+--   local cwd = vim.fn.getcwd()
+--   local candidate_path
+--   for _, venv in ipairs({ "venv", ".venv", "env" }) do
+--     candidate_path = string.format("%s/%s/bin/python", cwd, venv)
+--     if vim.fn.executable(candidate_path) == 1 then
+--       return venv
+--     end
+--   end
+--   return vim.env.HOME .. "/.asdf/shims/python3"
+-- end
 
 vim.lsp.start({
   name = "pyright",
--- config/nvim/plugin/lsp.lua
+++ config/nvim/plugin/lsp.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     local client = vim.lsp.get_client_by_id(args.data.client_id)
     -- vim.json.encode(client)
     -- vim.notify(vim.inspect(client.server_capabilities))
-    vim.notify(vim.inspect(client.name))
+    -- vim.notify(vim.inspect(client.name))
     -- vim.notify(vim.inspect(client.config))
 
     if client.server_capabilities.hoverProvider then
