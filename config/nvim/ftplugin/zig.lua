e1ab7d0e08fd98a17a559c0c96cba85696be837e
Jonathan Lopez <jonathanglopez@gmail.com>
2022-06-23 12:41:51 -0400

    Adding WIP Zig command

---
+++ config/nvim/ftplugin/zig.lua
@@ -0,0 +1,31 @@
+local Job = require("plenary.job")
+
+vim.bo.makeprg = "zig build %:S"
+
+vim.api.nvim_buf_create_user_command(0, "Zig", function(args)
+  local message = {}
+  Job
+    :new({
+      command = "zig",
+      args = { args.args, vim.api.nvim_buf_get_name(0) },
+      on_stderr = function(_, return_val)
+        -- vim.notify("Error")
+        -- vim.notify(vim.inspect(j), 4)
+      table.insert(message, return_val)
+        -- vim.notify(vim.inspect(return_val), 4)
+      end,
+      on_stdout = function(j, return_val)
+        vim.notify("stdout")
+        vim.notify(vim.inspect(j))
+        vim.notify(vim.inspect(return_val))
+      end,
+      on_exit = function(j, return_val)
+        if return_val == 0 then
+          local version = j:result()[1]
+          vim.notify(version)
+        end
+        vim.notify(vim.inspect(message))
+      end,
+    })
+    :start()
+end, { nargs = 1 })
