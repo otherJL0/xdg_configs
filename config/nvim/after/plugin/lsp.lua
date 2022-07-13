038c28afa4fd830ef9c37bd96498e3fa74608c34
Jonathan Lopez <jonathanglopez@gmail.com>
2022-07-13 16:35:17 -0400

    Update diagnostic settings

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -1,7 +1,7 @@
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics,
   {
-    virtual_text = false,
+    virtual_text = true,
     signs = true,
     underline = true,
     update_in_insert = true,
@@ -28,18 +28,6 @@ vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
 )
 
 vim.o.updatetime = 200
-
-vim.cmd([[
-  highlight LspDiagnosticsLineNrError guibg=#51202A guifg=#FF0000 gui=bold
-  highlight LspDiagnosticsLineNrWarning guibg=#51412A guifg=#FFA500 gui=bold
-  highlight LspDiagnosticsLineNrInformation guibg=#1E535D guifg=#00FFFF gui=bold
-  highlight LspDiagnosticsLineNrHint guibg=#1E205D guifg=#0000FF gui=bold
-
-  sign define DiagnosticSignError text= texthl=LspDiagnosticsSignError linehl= numhl=LspDiagnosticsLineNrError
-  sign define DiagnosticSignWarn text= texthl=LspDiagnosticsSignWarning linehl= numhl=LspDiagnosticsLineNrWarning
-  sign define DiagnosticSignInfo text= texthl=LspDiagnosticsSignInformation linehl= numhl=LspDiagnosticsLineNrInformation
-  sign define DiagnosticSignHint text= texthl=LspDiagnosticsSignHint linehl= numhl=LspDiagnosticsLineNrHint
-]])
 
 local language_servers = {
   'sumneko_lua',
