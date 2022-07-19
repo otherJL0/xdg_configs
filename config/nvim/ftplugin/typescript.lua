c660c46ae2ab8f92e392176fe871a4797d451a8b
Jonathan Lopez <jonathanglopez@gmail.com>
2022-07-19 10:08:46 -0400

    Apply deno configs for typescript files

--- config/nvim/ftplugin/typescript.lua
+++ config/nvim/ftplugin/typescript.lua
@@ -66,9 +66,92 @@ if node_root_dir then
 end
 
 if deno_root_dir then
+  local function buf_cache()
+    local params = {
+      referrer = {
+        uri = vim.uri_from_bufnr(0),
+      },
+      uris = {},
+    }
+    vim.lsp.buf_request(0, 'deno/cache', params, nil)
+  end
+
+  local function virtual_text_document_handler(uri, result)
+    if not result then
+      return nil
+    end
+
+    for _, res in pairs(result) do
+      -- Error might be present because of race, deno server will eventually send a result. #1995
+      if res.error ~= nil then
+        require('vim.lsp.log').warn(
+          'deno/virtual_text_document handler failed (might be a temporary issue), error: '
+            .. tostring(res.error)
+        )
+      else
+        local lines = vim.split(res.result, '\n')
+        local bufnr = vim.uri_to_bufnr(uri)
+
+        local current_buf = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+        if #current_buf ~= 0 then
+          return nil
+        end
+
+        vim.api.nvim_buf_set_lines(bufnr, 0, -1, nil, lines)
+        vim.api.nvim_buf_set_option(bufnr, 'readonly', true)
+        vim.api.nvim_buf_set_option(bufnr, 'modified', false)
+        vim.api.nvim_buf_set_option(bufnr, 'modifiable', false)
+      end
+    end
+  end
+
+  local function virtual_text_document(uri)
+    local params = {
+      textDocument = {
+        uri = uri,
+      },
+    }
+    local result = lsp.buf_request_sync(0, 'deno/virtualTextDocument', params)
+    virtual_text_document_handler(uri, result)
+  end
+
+  local function denols_handler(err, result, ctx)
+    if not result or vim.tbl_isempty(result) then
+      return nil
+    end
+
+    for _, res in pairs(result) do
+      local uri = res.uri or res.targetUri
+      if uri:match('^deno:') then
+        virtual_text_document(uri)
+        res['uri'] = uri
+        res['targetUri'] = uri
+      end
+    end
+
+    vim.lsp.handlers[ctx.method](err, result, ctx)
+    vim.lsp.handlers[ctx.method](err, result, ctx)
+  end
+
   vim.lsp.start({
     name = 'denols',
     cmd = { 'deno', 'lsp' },
     root_dir = deno_root_dir,
+    init_options = {
+      enable = true,
+      unstable = true,
+    },
+    handlers = {
+      ['textDocument/definition'] = denols_handler,
+      ['textDocument/references'] = denols_handler,
+    },
+    commands = {
+      DenolsCache = {
+        function()
+          buf_cache()
+        end,
+        description = 'Cache a module and all its dependencies',
+      },
+    },
   })
 end
