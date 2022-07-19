92cd1730e6c3ac641fe7c5d27bf24fa8b4269e0c
Jonathan Lopez <jonathanglopez@gmail.com>
2022-07-19 17:18:14 -0400

    Remove unnecessary comments

--- config/nvim/ftplugin/typescript.lua
+++ config/nvim/ftplugin/typescript.lua
@@ -9,13 +9,3 @@ local project_type = ftutil.detect_project_type()
 if project_type ~= nil then
   ftutil.setup_project[project_type.type](project_type.root_dir)
 end
-
--- if is_deno_project and is_node_project then
---   vim.notify('Both deno and node project root files detected!')
--- elseif is_deno_project then
---   ftutil.setup_deno_project()
--- elseif is_node_project then
---   ftutil.setup_node_project()
--- else
---   vim.notify('Neither deno nor node detected')
--- end
