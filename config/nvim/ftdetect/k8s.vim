76169696a637ed6fc2369faeafc2700d1ef38ad6
Jonathan Lopez <jonathanglopez@gmail.com>
2022-08-31 12:30:20 -0400

    Use treesitter to detect kubernetes yaml

--- config/nvim/ftdetect/k8s.vim
+++
@@ -1,1 +0,0 @@
-autocmd BufNewFile,BufRead *.yaml if search('^apiVersion:', 'nw') && search('^kind:', 'nw') | setlocal filetype=k8s | endif
--- config/nvim/ftplugin/yaml.lua
+++ config/nvim/ftplugin/yaml.lua
@@ -1,28 +1,77 @@
+vim.bo.shiftwidth = 2
+vim.bo.tabstop = 2
+vim.bo.expandtab = true
+
+local bufnr = vim.api.nvim_get_current_buf()
+local parser = vim.treesitter.get_parser(bufnr)
+local tree = parser:parse()
+local root = tree[1]:root()
+
+local function get_val(key)
+  local query = vim.treesitter.parse_query(
+    'yaml',
+    string.format(
+      [[
+;; query
+(block_mapping_pair 
+(flow_node (plain_scalar (string_scalar) @key_name_str))
+(flow_node (plain_scalar (string_scalar) @val_name_str))
+(#match? @key_name_str "^%s")
+) 
+]],
+      key
+    )
+  )
+
+  for id, match, metadata in query:iter_matches(root, bufnr) do
+    for j, key in ipairs(match) do
+      local capture_name = query.captures[j]
+      if capture_name == 'val_name_str' then
+        return vim.treesitter.get_node_text(key, bufnr)
+      end
+    end
+  end
+  return nil
+end
+
+local kubernetes = setmetatable({}, {
+  __index = function(self, key)
+    local _, res = pcall(get_val, key)
+    return res
+  end,
+})
+
+local yaml_configs = {
+  format = {
+    enable = true,
+    singleQuote = true,
+    bracketSpacing = true,
+    proseWrap = 'always',
+    printWidth = 80,
+  },
+  validate = true,
+  hover = true,
+  completion = true,
+  schemaStore = {
+    enable = true,
+  },
+  editor = {
+    formatOnType = true,
+  },
+}
+
+if kubernetes.kind ~= nil and kubernetes.apiVersion ~= nil then
+  yaml_configs.schemaStore.enable = false
+  yaml_configs.schemas = {
+    kubernetes = { '*' },
+  }
+end
+
 vim.lsp.start({
   name = 'yamlls',
   cmd = { 'yaml-language-server', '--stdio' },
   init_configs = {
-    yaml = {
-      format = {
-        enable = true,
-        singleQuote = true,
-        bracketSpacing = true,
-        proseWrap = 'always',
-        printWidth = 80,
-      },
-      validate = true,
-      hover = true,
-      completion = true,
-      schemaStore = {
-        enable = true,
-      },
-      -- schemas = {
-      --   kubernetes = { '*' },
-      -- },
-      editor = {
-        formatOnType = true,
-      },
-    },
+    yaml = yaml_configs,
   },
   -- before_init = function(initilize_params, config)
   --   -- vim.notify(vim.inspect(initilize_params))
