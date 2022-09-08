f9e8245969c7af6b0252797b9cf924743d6018df
Jonathan Lopez <jonathanglopez@gmail.com>
2022-09-08 10:04:39 -0400

    Update terraform-ls configs

--- config/nvim/ftplugin/terraform.lua
+++ config/nvim/ftplugin/terraform.lua
@@ -3,7 +3,18 @@ vim.bo.tabstop = 2
 vim.bo.softtabstop = 2
 vim.bo.shiftwidth = 2
 
+vim.lsp.set_log_level(0)
 vim.lsp.start({
   name = 'terraform-ls',
-  cmd = { 'terraform-ls', 'serve' },
+  cmd = {
+    'terraform-ls',
+    'serve',
+  },
+  init_options = {
+    ignoreSingleFileWarning = true,
+    experimentalFeatures = {
+      validateOnSave = true,
+      prefillRequiredFields = true,
+    }
+  },
 })
