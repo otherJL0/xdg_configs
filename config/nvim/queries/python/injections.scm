ce6d93a678c5f8a77545f176155fe5901d53ce63
Jonathan Lopez <jonathanglopez@gmail.com>
2022-09-13 15:46:57 -0400

    Adding python regex injection

--- config/nvim/queries/python/injections.scm
+++ config/nvim/queries/python/injections.scm
@@ -13,3 +13,12 @@ arguments: (argument_list (string) @sqlite (#offset! @sqlite 0 3 0 -3) (#match? 
 function: (attribute attribute: (identifier) @_execute)
 arguments: (argument_list (string) @sqlite (#offset! @sqlite 0 4 0 -4) (#match? @sqlite "^\"{3} [A-Za-z].*[\);] \"{3}$")) 
 (#match? @_execute "execute"))
+
+((call
+  function: (attribute
+	  object: (identifier) @_re)
+  arguments: (argument_list (string) @regex))
+ (#eq? @_re "re")
+ (#lua-match? @regex "^r.*"))
+
+(comment) @comment
