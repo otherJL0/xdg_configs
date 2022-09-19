692d6c66d6d665ef8ff36cc4d6ae3ed0ff4f427b
Jonathan Lopez <jonathanglopez@gmail.com>
2022-09-19 09:44:05 -0400

    Update clang flags on macos

--- config/fish/conf.d/llvm.fish
+++ config/fish/conf.d/llvm.fish
@@ -2,14 +2,13 @@
 # Clang
 set -gx CC clang
 set -gx CXX clang++
-# set -gx AOCC_HOME /opt/AMD/aocc-compiler-3.2.0
-# if test -d $AOCC_HOME
-#     set -gx CC "$AOCC_HOME/bin/clang -march=znver3"
-#     set -gx CXX "$AOCC_HOME/bin/clang++ -march=znver3"
-# else
-#     set -gx CC clang
-#     set -gx CXX clang++
-# end
+
+if test ( uname ) = Darwin
+  fish_add_path /opt/homebrew/opt/llvm/bin
+
+  set -gx LDFLAGS "-L/opt/homebrew/opt/llvm/lib -L/opt/homebrew/opt/llvm/lib/c++ -Wl,-rpath,/opt/homebrew/opt/llvm/lib/c++"
+  set -gx CPPFLAGS "-I/opt/homebrew/opt/llvm/include"
+end
 
 # CMake
 set -gx CMAKE_EXPORT_COMPILE_COMMANDS 1
@@ -19,7 +18,10 @@ set -gx CMAKE_C_COMPILER_LAUNCHER sccache
 set -gx CMAKE_CXX_COMPILER_LAUNCHER sccache
 
 
+# CCACHE
 set -gx CCACHE_MAXSIZE 100G
 set -gx CCACHE_DIR ~/.cache/ccache
 set -gx SCCACHE_MAXSIZE 100G
 set -gx SCCACHE_DIR ~/.cache/sccache
+
+
