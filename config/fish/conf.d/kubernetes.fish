3b2bde2a205c261a32abede39b3e2deaf9748d0a
Jonathan Lopez <jonathanglopez@gmail.com>
2022-09-22 10:35:27 -0400

    Adding kubernetes abbreviations

--- config/fish/conf.d/kubernetes.fish
+++ config/fish/conf.d/kubernetes.fish
@@ -1,5 +1,9 @@
 abbr -a k kubectl
 
+abbr -a kpods kubectl get pods
+abbr -a knames kubectl get namespaces
+abbr -a kjobs kubectl get jobs
+
 abbr -a konfig kubectl config
 abbr -a kontext kubectl config use-context
 abbr -a kontexts kubectl config get-contexts
