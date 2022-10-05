7c2c8395c71f4750b2684ed66fe2f99c1a674c06
Jonathan Lopez <jonathanglopez@gmail.com>
2022-10-05 11:53:17 -0400

    Update rust configs on m1 macos

--- config/fish/conf.d/rust.fish
+++ config/fish/conf.d/rust.fish
@@ -3,8 +3,8 @@
 set -gx RUSTUP_HOME $HOME/.rustup
 
 if test ( uname ) = Darwin
-    set -gx RUSTUP_TOOLCHAIN stable-x86_64-apple-darwin
-    fish_add_path --prepend --path --global $RUSTUP_HOME/toolchains/nightly-x86_64-apple-darwin/bin
+    set -gx RUSTUP_TOOLCHAIN stable-aarch64-apple-darwin
+    fish_add_path --prepend --path --global $RUSTUP_HOME/toolchains/nightly-aarch64-apple-darwin/bin
 else
     set -gx RUSTUP_TOOLCHAIN stable-x86_64-unknown-linux-gnu
     fish_add_path --prepend --path --global $RUSTUP_HOME/toolchains/nightly-x86_64-unknown-linux-gnu/bin
