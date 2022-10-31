1896edcded51984e8575ce8eb42cf90672ded3e1
Jonathan Lopez <jonathanglopez@gmail.com>
2022-10-31 18:02:17 -0400

    Use noice for document hover

--- config/nvim/lua/config/tui.lua
+++ config/nvim/lua/config/tui.lua
@@ -1,4 +1,11 @@
 require('noice').setup({
+  lsp = {
+    override = {
+      ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+      ['vim.lsp.util.stylize_markdown'] = true,
+      ['cmp.entry.get_documentation'] = true,
+    },
+  },
   views = {
     cmdline_popup = {
       position = {
