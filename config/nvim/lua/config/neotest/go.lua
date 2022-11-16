4de5b1c5a5a50de90261cfafd9ec1789efc2ece0
Jonathan Lopez <jonathanglopez@gmail.com>
2022-11-16 16:02:33 -0500

    Updating debug and test for golang

--- config/nvim/lua/config/neotest/go.lua
+++ config/nvim/lua/config/neotest/go.lua
@@ -1,7 +1,7 @@
 local neotest = require('neotest')
 local dap_go = require('dap-go')
 local dap = require('dap')
-local ui = require('dapui')
+local dapui = require('dapui')
 
 require('nvim-dap-virtual-text').setup({
   enabled = true, -- enable this plugin (the default)
@@ -18,14 +18,97 @@ require('nvim-dap-virtual-text').setup({
   -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
 })
 
-ui.setup()
+dapui.setup({
+  icons = { expanded = '', collapsed = '', current_frame = '*' },
+  mappings = {
+    -- Use a table to apply multiple mappings
+    expand = { '<CR>', '<2-LeftMouse>' },
+    open = 'o',
+    remove = 'd',
+    edit = 'e',
+    repl = 'r',
+    toggle = 't',
+  },
+  -- Use this to override mappings for specific elements
+  element_mappings = {
+    -- Example:
+    -- stacks = {
+    --   open = "<CR>",
+    --   expand = "o",
+    -- }
+  },
+  -- Expand lines larger than the window
+  -- Requires >= 0.7
+  expand_lines = vim.fn.has('nvim-0.7') == 1,
+  -- Layouts define sections of the screen to place windows.
+  -- The position can be "left", "right", "top" or "bottom".
+  -- The size specifies the height/width depending on position. It can be an Int
+  -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
+  -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
+  -- Elements are the elements shown in the layout (in order).
+  -- Layouts are opened in order so that earlier layouts take priority in window sizing.
+  layouts = {
+    {
+      elements = {
+        -- Elements can be strings or table with id and size keys.
+        { id = 'scopes', size = 0.25 },
+        'breakpoints',
+        'stacks',
+        'watches',
+      },
+      size = 40, -- 40 columns
+      position = 'left',
+    },
+    {
+      elements = {
+        'repl',
+        'console',
+      },
+      size = 0.25, -- 25% of total lines
+      position = 'bottom',
+    },
+  },
+  controls = {
+    -- Requires Neovim nightly (or 0.8 when released)
+    enabled = true,
+    -- Display controls in this element
+    element = 'repl',
+    icons = {
+      pause = '||',
+      play = '|>',
+      step_into = '->>',
+      step_over = '~>',
+      step_out = '<~',
+      step_back = '<<',
+      run_last = '<||',
+      terminate = '-|',
+    },
+  },
+  floating = {
+    max_height = nil, -- These can be integers or a float between 0 and 1.
+    max_width = nil, -- Floats will be treated as percentage of your screen.
+    border = 'single', -- Border style. Can be "single", "double" or "rounded"
+    mappings = {
+      close = { 'q', '<Esc>' },
+    },
+  },
+  windows = { indent = 1 },
+  render = {
+    max_type_length = nil, -- Can be integer or nil.
+    max_value_lines = 100, -- Can be integer or nil.
+  },
+})
+
+dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
 vim.keymap.set('n', '<F8>', dap.toggle_breakpoint)
 vim.keymap.set('n', '  bp', dap.toggle_breakpoint)
 vim.keymap.set('n', '<F9>', dap.continue)
 vim.keymap.set('n', '  c', dap.continue)
 vim.keymap.set('n', '<F7>', dap.reverse_continue)
-vim.keymap.set('n', '<F4>', ui.toggle)
+vim.keymap.set('n', '<F4>', dapui.toggle)
 vim.keymap.set('n', '<F2>', dap.step_over)
 vim.keymap.set('n', '<F1>', dap.step_into)
 vim.keymap.set('n', ' <F5>', dap.terminate)
