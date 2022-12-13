a6d492c26c0e57c5c34de085f7df3fe3c72c0400
Jonathan Lopez <jonathanglopez@gmail.com>
2022-12-13 09:52:19 -0500

    Clean up lsp configs

---
+++ config/nvim/lua/config/lsp/init.lua
@@ -0,0 +1,14 @@
+local lspconfig = setmetatable({}, {
+  __index = function(_, server)
+    local config = require(string.format("config.lsp.%s", server))
+    -- nvim-cmp supports additional completion capabilities
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+    config.capabilities = capabilities
+    config.on_attach = require("config.lsp.on_attach").on_attach
+    return config
+  end,
+})
+
+
+return lspconfig
