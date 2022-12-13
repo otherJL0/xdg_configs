8597de1b88136581ffc007516ed699017e81e650
Jonathan Lopez <jonathanglopez@gmail.com>
2022-12-13 09:29:20 -0500

    Update cmp config

--- config/nvim/lua/config/ui/cmp.lua
+++ config/nvim/lua/config/ui/cmp.lua
@@ -10,6 +10,24 @@ cmp.setup({
     -- can be "custom", "wildmenu" or "native"
     entries = "custom",
   },
+  window = {
+    completion = {
+      winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+      col_offset = -3,
+      side_padding = 0,
+    },
+  },
+  formatting = {
+    fields = { "kind", "abbr", "menu" },
+    format = function(entry, vim_item)
+      local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+      local strings = vim.split(kind.kind, "%s", { trimempty = true })
+      kind.kind = " " .. strings[1] .. " "
+      kind.menu = "    (" .. strings[2] .. ")"
+
+      return kind
+    end,
+  },
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
