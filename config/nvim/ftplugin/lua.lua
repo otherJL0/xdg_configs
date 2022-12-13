235bf12cb6a8b4b183163c2651af4462805248ac
Jonathan Lopez <jonathanglopez@gmail.com>
2022-12-13 09:29:53 -0500

    Setup lua-language-server

--- config/nvim/ftplugin/lua.lua
+++ config/nvim/ftplugin/lua.lua
@@ -1,3 +1,37 @@
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+
+require("neodev").setup({
+  library = {
+    enabled = true, -- when not enabled, neodev will not change any settings to the LSP server
+    -- these settings will be used for your Neovim config directory
+    runtime = true, -- runtime path
+    types = true, -- full signature, docs and completion of vim.api, vim.treesitter, vim.lsp and others
+    plugins = true, -- installed opt or start plugins in packpath
+    -- you can also specify the list of plugins to make available as a workspace library
+    -- plugins = { "nvim-treesitter", "plenary.nvim", "telescope.nvim" },
+  },
+  setup_jsonls = true, -- configures jsonls to provide completion for project specific .luarc.json files
+  -- for your Neovim config directory, the config.library settings will be used as is
+  -- for plugin directories (root_dirs having a /lua directory), config.library.plugins will be disabled
+  -- for any other directory, config.library.enabled will be set to false
+  -- override = function(root_dir, options) end,
+  -- With lspconfig, Neodev will automatically setup your lua-language-server
+  -- If you disable this, then you have to set {before_init=require("neodev.lsp").before_init}
+  -- in your lsp start options
+  lspconfig = false,
+})
+
+vim.lsp.start({
+  name = "lua-language-server",
+  cmd = { "lua-language-server" },
+  before_init = require("neodev.lsp").before_init,
+  root_dir = vim.fs.dirname(
+    vim.fs.find(
+      { ".luarc.json", "luarc.jsonc", "stylua.toml", ".stylua.toml", "selene.toml", "selene.yml", "selene.yaml" },
+      { upward = true }
+    )[1]
+  ),
+  on_attach = require("config.lsp.on_attach").on_attach,
+})
