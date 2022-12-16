be7aa0bc624b7bb5d0dc315942e0ea170e9450dc
Jonathan Lopez <jonathanglopez@gmail.com>
2022-12-16 08:29:44 -0500

    Updating global options and keybindings

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -18,6 +18,31 @@ vim.opt.smartcase = true -- Except when explicitly adding uppercase
 
 vim.opt.inccommand = "nosplit" -- Highlight search/replace queries
 vim.opt.showmatch = true -- Show matching bracket character when hovering
+vim.opt.signcolumn = "yes:3" -- Always show a sign column
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.keymap.set("n", [[  "]], ":split<CR>")
+vim.keymap.set("n", [[  %]], ":vsplit<CR>")
+
+vim.keymap.set({ "i", "c" }, "<C-B>", "<Left>")
+vim.keymap.set({ "i", "c" }, "<C-F>", "<Right>")
+vim.keymap.set({ "i", "c" }, "<C-D>", "<Del>")
+vim.keymap.set({ "i", "c" }, "<C-P>", "<Up>")
+vim.keymap.set({ "i", "c" }, "<C-N>", "<Down>")
+vim.keymap.set({ "i", "c" }, "<C-A>", "<Home>")
+vim.keymap.set({ "i", "c" }, "<C-E>", "<End>")
+vim.keymap.set({ "i", "c" }, "<A-b>", "<S-Left>")
+vim.keymap.set({ "i", "c" }, "<A-f>", "<S-Right>")
+vim.keymap.set({ "i", "c" }, "<A-x>", "<Esc>:")
+vim.keymap.set({ "i", "c" }, "<A-f>", "<Esc>")
+vim.keymap.set({ "i", "c" }, "<A-f>", "<S-Right>")
+vim.keymap.set({ "i", "c" }, "<A-f>", "<S-Right>")
+
+--
+--
+
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = "*",
