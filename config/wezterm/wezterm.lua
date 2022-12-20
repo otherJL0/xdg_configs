0fc72f327f89664c5b3fa293424de3b34b523576
Jonathan Lopez <jonathanglopez@gmail.com>
2022-12-20 09:06:33 -0500

    Update wezterm configs

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,6 +1,10 @@
 local wezterm = require 'wezterm'
+local color_scheme = wezterm.color.get_builtin_schemes()['Catppuccin Mocha']
+print(color_scheme)
 
 return {
   font = wezterm.font 'JetBrains Mono',
-	color_scheme = "Catppuccin Mocha",
+	color_scheme = 'Catppuccin Mocha',
+  enable_tab_bar = false,
+  term = "wezterm",
 }
