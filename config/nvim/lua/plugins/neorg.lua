12c9956752e094e763410e38f96bd47314636ccb
Jonathan Lopez <jonathanglopez@gmail.com>
2023-07-21 11:22:46 -0400

    Adding norg plugin

---
+++ config/nvim/lua/plugins/neorg.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "nvim-neorg/neorg",
+    lazy = true,
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-neorg/neorg-telescope" },
+    opts = {
+      load = {
+        ["core.defaults"] = {}, -- Loads default behaviour
+        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.integrations.telescope"] = {},
+        ["core.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notes = "~/neorg",
+            },
+          },
+        },
+      },
+    },
+  },
+}
