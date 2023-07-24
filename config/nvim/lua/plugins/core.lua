4933e6631ddd34c92e456a843be8403c6512640e
Jonathan Lopez <jonathanglopez@gmail.com>
2023-07-24 16:52:22 -0400

    Update lazy configs

--- config/nvim/lua/plugins/core.lua
+++ config/nvim/lua/plugins/core.lua
@@ -1,5 +1,13 @@
 return {
   { "folke/lazy.nvim", version = false },
   { "LazyVim/LazyVim", version = false },
-  { "neovim/nvim-lspconfig", opts = { inlay_hints = { enabled = true } } },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        zls = {},
+      },
+      inlay_hints = { enabled = true },
+    },
+  },
 }
