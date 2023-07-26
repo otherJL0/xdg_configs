291906f045fedd4739cb38b94295d9a8d8085e05
Jonathan Lopez <jonathanglopez@gmail.com>
2023-07-26 08:28:23 -0400

    Fix typo

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -4,7 +4,7 @@
 vim.opt.list = true
 vim.opt.listchars:append("space:⋅")
 vim.opt.listchars:append("eol:↴")
-vim.opt.listchars:append("diff:/")
+vim.opt.fillchars:append({ diff = "╱" })
 
 vim.o.winwidth = 10
 vim.o.winminwidth = 10
