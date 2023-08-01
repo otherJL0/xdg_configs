de11d5f4dcae402b41b9bec09175f369a196bab3
Jonathan Lopez <jonathanglopez@gmail.com>
2023-08-01 10:02:05 -0400

    Update neorg configs

--- config/nvim/lua/plugins/neorg.lua
+++ config/nvim/lua/plugins/neorg.lua
@@ -1,14 +1,14 @@
 return {
   {
     "nvim-neorg/neorg",
-    lazy = true,
+    ft = { "neorg", "norg" },
     build = ":Neorg sync-parsers",
     dependencies = { "nvim-lua/plenary.nvim", "nvim-neorg/neorg-telescope" },
     opts = {
       load = {
         ["core.defaults"] = {}, -- Loads default behaviour
         ["core.concealer"] = {}, -- Adds pretty icons to your documents
-        ["core.integrations.telescope"] = {},
+        -- ["core.integrations.telescope"] = {},
         ["core.dirman"] = { -- Manages Neorg workspaces
           config = {
             workspaces = {
